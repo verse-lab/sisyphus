ff664ee28bc739173d3114e6b474b0edbf587046
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-05-06 12:50:15 +0100

    added print endlines

--- lib/proof_parser/main.ml
+++ lib/proof_parser/main.ml
@@ -32,18 +32,43 @@ let statement_0 = List.hd asts
 
 let () =
   match statement_0 with
-| Serapi.Serapi_protocol.CoqString _ |Serapi.Serapi_protocol.CoqSList _
-|Serapi.Serapi_protocol.CoqPp _ |Serapi.Serapi_protocol.CoqLoc _
-|Serapi.Serapi_protocol.CoqTok _ |Serapi.Serapi_protocol.CoqDP _
-|Serapi.Serapi_protocol.CoqAst _ |Serapi.Serapi_protocol.CoqOption (_, _)
-|Serapi.Serapi_protocol.CoqConstr _ |Serapi.Serapi_protocol.CoqExpr _
-|Serapi.Serapi_protocol.CoqMInd (_, _) |Serapi.Serapi_protocol.CoqEnv _
-|Serapi.Serapi_protocol.CoqTactic (_, _) |Serapi.Serapi_protocol.CoqLtac _
-|Serapi.Serapi_protocol.CoqGenArg _ |Serapi.Serapi_protocol.CoqQualId _
-|Serapi.Serapi_protocol.CoqGlobRef _ |Serapi.Serapi_protocol.CoqGlobRefExt _
-|Serapi.Serapi_protocol.CoqImplicit _ |Serapi.Serapi_protocol.CoqProfData _
-|Serapi.Serapi_protocol.CoqNotation _ |Serapi.Serapi_protocol.CoqUnparsing
-(_, _, _) |Serapi.Serapi_protocol.CoqGoal _
-|Serapi.Serapi_protocol.CoqExtGoal _ |Serapi.Serapi_protocol.CoqProof 
-(_, _) |Serapi.Serapi_protocol.CoqAssumptions _
-|Serapi.Serapi_protocol.CoqComments _ -> print_endline "hello world"
+  | Serapi.Serapi_protocol.CoqString _ -> print_endline "CoqString"
+  |Serapi.Serapi_protocol.CoqSList _ -> print_endline "CoqSList"
+                                          
+  |Serapi.Serapi_protocol.CoqPp _ -> print_endline "CoqPp"
+  |Serapi.Serapi_protocol.CoqLoc _ -> print_endline "CoqLoc"
+                                        
+  |Serapi.Serapi_protocol.CoqTok _ -> print_endline "CoqTok"
+  |Serapi.Serapi_protocol.CoqDP _ -> print_endline "CoqDP"
+                                       
+  |Serapi.Serapi_protocol.CoqAst _ -> print_endline "CoqAst"
+  |Serapi.Serapi_protocol.CoqOption (_ , _) -> print_endline "CoqOption"
+  |Serapi.Serapi_protocol.CoqConstr _ -> print_endline "CoqConstr"
+  |Serapi.Serapi_protocol.CoqExpr _
+    -> print_endline "CoqExpr"
+  |Serapi.Serapi_protocol.CoqMInd (_, _) -> print_endline "CoqMInd"
+  |Serapi.Serapi_protocol.CoqEnv _
+    -> print_endline "CoqEnv"
+  |Serapi.Serapi_protocol.CoqTactic (_, _) -> print_endline "CoqTactic"
+  |Serapi.Serapi_protocol.CoqLtac _
+    -> print_endline "CoqLtac"
+  |Serapi.Serapi_protocol.CoqGenArg _ -> print_endline "CoqGenArg"
+  |Serapi.Serapi_protocol.CoqQualId _
+    -> print_endline "CoqQualId"
+  |Serapi.Serapi_protocol.CoqGlobRef _ -> print_endline "CoqGlobRef"
+  |Serapi.Serapi_protocol.CoqGlobRefExt _
+    -> print_endline "CoqGlobRefExt"
+  |Serapi.Serapi_protocol.CoqImplicit _ -> print_endline "CoqImplicit"
+  |Serapi.Serapi_protocol.CoqProfData _
+    -> print_endline "CoqProfData"
+  |Serapi.Serapi_protocol.CoqNotation _ -> print_endline "CoqNotation"
+  |Serapi.Serapi_protocol.CoqUnparsing
+     (_, _, _) -> print_endline "CoqUnparsing"
+  |Serapi.Serapi_protocol.CoqGoal _
+    -> print_endline "CoqGoal"
+  |Serapi.Serapi_protocol.CoqExtGoal _ -> print_endline "CoqExtGoal"
+  |Serapi.Serapi_protocol.CoqProof 
+     (_, _) -> print_endline "CoqProof"
+  |Serapi.Serapi_protocol.CoqAssumptions _
+    -> print_endline "CoqAssumptions"
+  |Serapi.Serapi_protocol.CoqComments _ -> print_endline "CoqComments"
