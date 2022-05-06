68478e93dca8d24278b92793c5fdf06f1720f775
mkeoliya <e0407889@u.nus.edu>
2022-05-06 20:08:04 +0800

    Add vernac-match expressions

--- lib/proof_parser/main.ml
+++ lib/proof_parser/main.ml
@@ -25,50 +25,60 @@ let asts =
    Ctx.query ~at Serapi.Serapi_protocol.Ast 
   )
   |> Iter.flat_map_l Fun.id
+  |> Iter.filter_map (function Serapi.Serapi_protocol.CoqAst v ->
+                      
+                      Some v.v
+                      | _ -> None
+                      )
   |> Iter.to_list
 
 let statement_0 = List.hd asts
 
 
 let () =
-  match statement_0 with
-  | Serapi.Serapi_protocol.CoqString _ -> print_endline "CoqString"
-  |Serapi.Serapi_protocol.CoqSList _ -> print_endline "CoqSList"
-                                          
-  |Serapi.Serapi_protocol.CoqPp _ -> print_endline "CoqPp"
-  |Serapi.Serapi_protocol.CoqLoc _ -> print_endline "CoqLoc"
-                                        
-  |Serapi.Serapi_protocol.CoqTok _ -> print_endline "CoqTok"
-  |Serapi.Serapi_protocol.CoqDP _ -> print_endline "CoqDP"
-                                       
-  |Serapi.Serapi_protocol.CoqAst _ -> print_endline "CoqAst"
-  |Serapi.Serapi_protocol.CoqOption (_ , _) -> print_endline "CoqOption"
-  |Serapi.Serapi_protocol.CoqConstr _ -> print_endline "CoqConstr"
-  |Serapi.Serapi_protocol.CoqExpr _
-    -> print_endline "CoqExpr"
-  |Serapi.Serapi_protocol.CoqMInd (_, _) -> print_endline "CoqMInd"
-  |Serapi.Serapi_protocol.CoqEnv _
-    -> print_endline "CoqEnv"
-  |Serapi.Serapi_protocol.CoqTactic (_, _) -> print_endline "CoqTactic"
-  |Serapi.Serapi_protocol.CoqLtac _
-    -> print_endline "CoqLtac"
-  |Serapi.Serapi_protocol.CoqGenArg _ -> print_endline "CoqGenArg"
-  |Serapi.Serapi_protocol.CoqQualId _
-    -> print_endline "CoqQualId"
-  |Serapi.Serapi_protocol.CoqGlobRef _ -> print_endline "CoqGlobRef"
-  |Serapi.Serapi_protocol.CoqGlobRefExt _
-    -> print_endline "CoqGlobRefExt"
-  |Serapi.Serapi_protocol.CoqImplicit _ -> print_endline "CoqImplicit"
-  |Serapi.Serapi_protocol.CoqProfData _
-    -> print_endline "CoqProfData"
-  |Serapi.Serapi_protocol.CoqNotation _ -> print_endline "CoqNotation"
-  |Serapi.Serapi_protocol.CoqUnparsing
-     (_, _, _) -> print_endline "CoqUnparsing"
-  |Serapi.Serapi_protocol.CoqGoal _
-    -> print_endline "CoqGoal"
-  |Serapi.Serapi_protocol.CoqExtGoal _ -> print_endline "CoqExtGoal"
-  |Serapi.Serapi_protocol.CoqProof 
-     (_, _) -> print_endline "CoqProof"
-  |Serapi.Serapi_protocol.CoqAssumptions _
-    -> print_endline "CoqAssumptions"
-  |Serapi.Serapi_protocol.CoqComments _ -> print_endline "CoqComments"
+  match statement_0.expr with
+  | Vernacexpr.VernacLoad (_, _) |Vernacexpr.VernacReservedNotation (_, _)
+|Vernacexpr.VernacOpenCloseScope (_, _) |Vernacexpr.VernacDeclareScope _
+|Vernacexpr.VernacDelimiters (_, _) |Vernacexpr.VernacBindScope (_, _)
+|Vernacexpr.VernacNotation (_, _, _, _) |Vernacexpr.VernacNotationAddFormat
+(_, _, _) |Vernacexpr.VernacDeclareCustomEntry _ |Vernacexpr.VernacDefinition
+(_, _, _) |Vernacexpr.VernacStartTheoremProof (_, _)
+|Vernacexpr.VernacEndProof _ |Vernacexpr.VernacExactProof _
+|Vernacexpr.VernacAssumption (_, _, _) |Vernacexpr.VernacInductive (_, _)
+|Vernacexpr.VernacFixpoint (_, _) |Vernacexpr.VernacCoFixpoint (_, _)
+|Vernacexpr.VernacScheme _ |Vernacexpr.VernacCombinedScheme (_, _)
+|Vernacexpr.VernacUniverse _ |Vernacexpr.VernacConstraint _
+|Vernacexpr.VernacBeginSection _ |Vernacexpr.VernacEndSegment _
+|Vernacexpr.VernacRequire (_, _, _) |Vernacexpr.VernacImport (_, _, _)
+|Vernacexpr.VernacCanonical _ |Vernacexpr.VernacCoercion (_, _, _)
+|Vernacexpr.VernacIdentityCoercion (_, _, _)
+|Vernacexpr.VernacNameSectionHypSet (_, _) |Vernacexpr.VernacInstance
+(_, _, _, _, _) |Vernacexpr.VernacDeclareInstance (_, _, _, _)
+|Vernacexpr.VernacContext _ |Vernacexpr.VernacExistingInstance _
+|Vernacexpr.VernacExistingClass _ |Vernacexpr.VernacDeclareModule
+(_, _, _, _) |Vernacexpr.VernacDefineModule (_, _, _, _, _)
+|Vernacexpr.VernacDeclareModuleType (_, _, _, _) |Vernacexpr.VernacInclude _
+|Vernacexpr.VernacAddLoadPath _ |Vernacexpr.VernacRemoveLoadPath _
+|Vernacexpr.VernacAddMLPath _ |Vernacexpr.VernacDeclareMLModule _
+|Vernacexpr.VernacChdir _ |Vernacexpr.VernacResetName _
+|Vernacexpr.VernacResetInitial |Vernacexpr.VernacBack _
+|Vernacexpr.VernacCreateHintDb (_, _) |Vernacexpr.VernacRemoveHints (_, _)
+|Vernacexpr.VernacHints (_, _) |Vernacexpr.VernacSyntacticDefinition
+(_, _, _) |Vernacexpr.VernacArguments (_, _, _, _) |Vernacexpr.VernacReserve
+_ |Vernacexpr.VernacGeneralizable _ |Vernacexpr.VernacSetOpacity _
+|Vernacexpr.VernacSetStrategy _ 
+|Vernacexpr.VernacAddOption (_, _) |Vernacexpr.VernacRemoveOption (_, _)
+|Vernacexpr.VernacMemOption (_, _) |Vernacexpr.VernacPrintOption _
+|Vernacexpr.VernacCheckMayEval (_, _, _) |Vernacexpr.VernacGlobalCheck _
+|Vernacexpr.VernacDeclareReduction (_, _) |Vernacexpr.VernacPrint _
+|Vernacexpr.VernacSearch (_, _, _) |Vernacexpr.VernacLocate _
+|Vernacexpr.VernacRegister (_, _) |Vernacexpr.VernacPrimitive (_, _, _)
+|Vernacexpr.VernacComments _ |Vernacexpr.VernacAbort _
+|Vernacexpr.VernacAbortAll |Vernacexpr.VernacRestart |Vernacexpr.VernacUndo _
+|Vernacexpr.VernacUndoTo _ |Vernacexpr.VernacFocus _
+|Vernacexpr.VernacUnfocus |Vernacexpr.VernacUnfocused
+|Vernacexpr.VernacBullet _ |Vernacexpr.VernacSubproof _
+|Vernacexpr.VernacEndSubproof |Vernacexpr.VernacShow _
+|Vernacexpr.VernacCheckGuard |Vernacexpr.VernacProof (_, _)
+|Vernacexpr.VernacProofMode _ |Vernacexpr.VernacExtend (_, _) -> print_endline "hello world"
+|Vernacexpr.VernacSetOption (_, _, _) -> print_endline "Found set option"
