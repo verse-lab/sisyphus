34200399f07ed10733d9f21546eb445ccd5ccbfe
mkeoliya <e0407889@u.nus.edu>
2022-05-06 19:47:04 +0800

    Remove old files, and add skeleton for new parser

--- lib/proof_parser/main.ml
+++ lib/proof_parser/main.ml
@@ -1,67 +1,49 @@
-[@@@warning "-33"]
 open Containers
 
-let iter_until_eof str f =
-  let lexbuf = Sedlexing.Utf8.from_string str in
-  let rec loop () =
-    match Proof_parser.Lexer.token lexbuf with
-    | EOF -> f Proof_parser.Raw_parser.EOF
-    | tok -> f tok; loop () in
-  loop ()
+let proof_str = IO.with_in "../../resources/seq_to_array/Verify_seq_to_array_old.v" IO.read_all
+
+let () = print_endline @@ "Hello world" ^ proof_str
+
+let asts =
+  let module Ctx =
+    Coq.Proof.Make(struct
+      let verbose = false
+      let libs = [
+        Coq.Coqlib.make
+          ~path:(Fpath.of_string "../../_build/default/resources/seq_to_array" |> Result.get_exn)
+          "Proofs"
+      ] 
+    end) in
+  Ctx.add proof_str;
+  Ctx.exec();
+
+  let start = Ctx.size() - 1 in
+
+  (* TODO: Candidate for function*)
+  Iter.int_range_by ~step:(-1) start 0
+  |> Iter.filter_map (fun at ->
+   Ctx.query ~at Serapi.Serapi_protocol.Ast 
+  )
+  |> Iter.flat_map_l Fun.id
+  |> Iter.to_list
+
+let statement_0 = List.hd asts
 
 
 let () =
-  let txt = {|
-Set Implicit Arguments.
-
-From CFML Require Import WPLib Stdlib.
-From TLC Require Import LibListZ.
-
-From Proofs Require Import Verify_seq_to_array_utils_new.
-From Proofs Require Import Seq_to_array_new.
-
-
-Lemma to_array_spec : forall (A: Type) `{EA:Enc A} (l:list A) (s:func),
-    SPEC (to_array s)
-    PRE (\[LSeq l s])
-    POST (fun (a: loc) => a ~> Array l).
-Proof using.
-  xcf.
-  xpullpure H1. 
-  xpurefun f Hf using (fun (f: func) =>
-     forall (i : credits) (acc : list A) (x : A),
-       SPEC (f (i,acc) x)
-       PRE (\[])
-       POST (fun (res: int * list A) => \[res = (i + 1, x :: acc)])).
-  xapp (fold_spec f (0, nil) s l (fun '((i,acc) : int * list A) (x: A) => (i + 1, x :: acc))); auto;
-    first sep_solve.
-  xdestruct len ls Hlenls.
-  rewrite list_fold_length_rev in Hlenls.
-  sep_split_tuple Hlenls Hlen Hls.
-  case l as [| x xs] eqn:H.
-  - xmatch_case_0.
-    xvalemptyarr.
-  - xmatch_case_1 with init rest Hrest.
-    xalloc arr data Hdata.
-    xletopaque idx Hidx.
-    xletopaque ftmp Hftmp.
-    xapp (list_fold_spec ftmp idx rest (fun (t: list A) (i: int) =>
-        \[i = length (x :: xs) - length t - 2] \*
-        arr ~> Array ((make (i + 1) init) ++ 
-                                 drop (i + 1) (x :: xs))
-         )). { admit. } { admit. } { admit. }
-   intros i Hi.
-   xdestruct.
-   xvals.
-   { admit. }
-Admitted.
-|} in
-
-  try
-  print_endline @@  
-  Proof_parser.Proof.show (Proof_parser.Parser.parse_str txt)
-  with exn ->
-    print_endline @@ Printexc.to_string exn;
-    iter_until_eof txt (fun tok -> print_endline @@ Proof_parser.Raw_parser.show_token tok);
-    
-
+  match statement_0 with
+| Serapi.Serapi_protocol.CoqString _ |Serapi.Serapi_protocol.CoqSList _
+|Serapi.Serapi_protocol.CoqPp _ |Serapi.Serapi_protocol.CoqLoc _
+|Serapi.Serapi_protocol.CoqTok _ |Serapi.Serapi_protocol.CoqDP _
+|Serapi.Serapi_protocol.CoqAst _ |Serapi.Serapi_protocol.CoqOption (_, _)
+|Serapi.Serapi_protocol.CoqConstr _ |Serapi.Serapi_protocol.CoqExpr _
+|Serapi.Serapi_protocol.CoqMInd (_, _) |Serapi.Serapi_protocol.CoqEnv _
+|Serapi.Serapi_protocol.CoqTactic (_, _) |Serapi.Serapi_protocol.CoqLtac _
+|Serapi.Serapi_protocol.CoqGenArg _ |Serapi.Serapi_protocol.CoqQualId _
+|Serapi.Serapi_protocol.CoqGlobRef _ |Serapi.Serapi_protocol.CoqGlobRefExt _
+|Serapi.Serapi_protocol.CoqImplicit _ |Serapi.Serapi_protocol.CoqProfData _
+|Serapi.Serapi_protocol.CoqNotation _ |Serapi.Serapi_protocol.CoqUnparsing
+(_, _, _) |Serapi.Serapi_protocol.CoqGoal _
+|Serapi.Serapi_protocol.CoqExtGoal _ |Serapi.Serapi_protocol.CoqProof 
+(_, _) |Serapi.Serapi_protocol.CoqAssumptions _
+|Serapi.Serapi_protocol.CoqComments _ -> print_endline "hello world"
