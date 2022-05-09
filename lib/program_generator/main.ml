cb88ef0c033b39815407f0d03d4a2f39042c4a6d
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-05-09 04:44:59 +0100

    updated interface for dynamic tracer

--- lib/program_generator/main.ml
+++ lib/program_generator/main.ml
@@ -1,9 +1,21 @@
 [@@@warning "-33"]
+open Containers
+
+let old_program =
+  IO.with_in "../../_build/default/resources/seq_to_array/seq_to_array_new.ml" IO.read_all
+  |> Lang.Sanitizer.parse_str
+
+let new_program =
+  IO.with_in "../../_build/default/resources/seq_to_array/seq_to_array_new.ml" IO.read_all
+  |> Lang.Sanitizer.parse_str
+
+
 
 let () =
   let module Ctx = (val Coq.Context.make [
-    Coq.Coqlib.make ~path:(Fpath.of_string "../../_build/default/resources/seq_to_array/" |> Result.get_ok) "Proofs"
+    Coq.Coqlib.make ~path:(Fpath.of_string "../../_build/default/resources/seq_to_array/" |> Result.get_exn) "Proofs"
   ]) in
 
+  
   
   print_endline "hello world!"
