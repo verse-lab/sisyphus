9a9380b464bd8012ce82b89a890aa4aede070e4d
mkeoliya <e0407889@u.nus.edu>
2022-05-11 15:01:14 +0800

    Add parser support for simple tactics

--- lib/proof_parser/main.ml
+++ lib/proof_parser/main.ml
@@ -1,10 +1,17 @@
 open Containers
+open Proof_spec.Script
+
+type proof = step list
+type script = {
+  directives: string;
+  name: string;
+  spec: string;
+  proof: proof;
+}
 
 let proof_str = IO.with_in "../../resources/seq_to_array/Verify_seq_to_array_old.v" IO.read_all
 
-let () = print_endline @@ "Hello world" ^ proof_str
-
-let print_tag = function
+let get_tag = function
   | Vernacexpr.VernacLoad (_, _) -> "VernacLoad"
   |Vernacexpr.VernacReservedNotation (_, _)
     -> "VernacReservedNotation"
@@ -88,7 +95,7 @@ let print_tag = function
   |Vernacexpr.VernacGeneralizable _ -> "VernacGeneralizable"
   |Vernacexpr.VernacSetOpacity _
     -> "VernacSetOpacity"
-  |Vernacexpr.VernacSetStrategy _ 
+  |Vernacexpr.VernacSetStrategy _
     -> "VernacSetStrategy"
   |Vernacexpr.VernacAddOption (_, _) -> "VernacAddOption"
   |Vernacexpr.VernacRemoveOption (_, _)
@@ -131,12 +138,32 @@ let print_tag = function
   |Vernacexpr.VernacProof (_, _)
     -> "VernacProof"
   |Vernacexpr.VernacProofMode _ -> "VernacProofMode"
-  |Vernacexpr.VernacExtend (_, _) -> "VernacExtend"
+  |Vernacexpr.VernacExtend (extend_name, args) ->
+    let f arg =
+      let s = Serlib.Ser_genarg.sexp_of_raw_generic_argument arg in
+      print_endline @@ Format.sprintf "%a" Sexplib.Sexp.pp_hum s;
+
+      (* let s2 = match s with_in | Sexplib.Sexp.Atom _ -> assert false | Sexplib.Sexp.List xs -> List.nth xs 3 in *)
+      (* let a2 = Serapi.Serapi_protocol.CoqGenArg (Serlib.Ser_genarg.raw_generic_argument_of_sexp (Sexplib.Sexp.Atom "xcf")) in *)
+
+      (* let a = Serapi.Serapi_protocol.CoqGenArg (Serlib.Ser_genarg.raw_generic_argument_of_sexp s) in *)
+      (* (\* match arg with *\) *)
+      (* | Genarg.GenArg (Genarg.Rawwit (Genarg.ExtraArg _), _) -> *)
+      (*   print_endline @@ Format.sprintf "%a" Pp.pp_with (Serapi.Serapi_protocol.gen_pp_obj Environ.empty_env Evd.empty a); *)
+      (* | _ -> () *)
+
+      let s2 =  Ppvernac.pr_vernac_expr (Vernacexpr.VernacExtend (extend_name, args)) in 
+      print_endline @@ Format.sprintf "%a" Pp.pp_with s2;
+
+    in
+
+    (* f (List.nth args 2); *)
+
+    "VernacExtend"
   |Vernacexpr.VernacSetOption (_, _, _) -> "VernacSetOption"
 
 
-=======
->>>>>>> parent of 28c80d5... added print_tag function
+
 let asts =
   let module Ctx =
     Coq.Proof.Make(struct
@@ -148,70 +175,124 @@ let asts =
       ] 
     end) in
   Ctx.add proof_str;
-  Ctx.exec();
+  (* Ctx.exec(); *)
 
   let start = Ctx.size() - 1 in
 
   (* TODO: Candidate for function*)
   Iter.int_range_by ~step:(-1) start 0
   |> Iter.filter_map (fun at ->
-   Ctx.query ~at Serapi.Serapi_protocol.Ast 
+
+   Ctx.query ~at Serapi.Serapi_protocol.Ast
   )
   |> Iter.flat_map_l Fun.id
   |> Iter.filter_map (function Serapi.Serapi_protocol.CoqAst v as ast ->
-    Format.printf "%7s, %a\n" (print_tag v.v.expr) Pp.pp_with (Serapi.Serapi_protocol.gen_pp_obj Environ.empty_env Evd.empty ast);
-    Some v.v
+    (* print_endline @@ Format.sprintf "%7s\n%a\n------------------%!" (get_tag v.v.expr) Pp.pp_with (Serapi.Serapi_protocol.gen_pp_obj Environ.empty_env Evd.empty ast); *)
+    Some v
                              | _ -> None
   )
   |> Iter.to_list
 
 let statement_0 = List.hd asts
 
+let ast_to_string ast =
+  let pp = (Serapi.Serapi_protocol.gen_pp_obj Environ.empty_env Evd.empty (Serapi.Serapi_protocol.CoqAst ast)) in
+  Format.sprintf "%a\n" Pp.pp_with pp
+
+let handle_decs asts  =
+  let is_dec (stmt: Vernacexpr.vernac_control_r CAst.t) =
+    match stmt.v.expr  with
+    | Vernacexpr.VernacSetOption (_, _, _) |  Vernacexpr.VernacRequire (_, _, _) -> true
+    | _ -> false
+  in
+  let decs, rest = List.partition is_dec asts in
+  let dec_str = List.fold_left (^) "" (List.map ast_to_string decs) in
+  dec_str, rest
+
+let handle_spec asts =
+  ast_to_string @@ List.hd asts, List.tl asts
+
+let vexpr_to_string vexpr =
+   Format.sprintf "%a" Pp.pp_with (Ppvernac.pr_vernac_expr vexpr)
+
+let tactics = ["xcf"; "xpullpure"; "xpurefun"; "xapp"; "xdestruct"; "rewrite"; "desstruct"; "xmatch_case"; "xvalemptyarr"; "xalloc"; "xletopaque"; "xvals"; "apply"; "intros"; "admitted"]
+
+let get_tactic str vexpr  =
+  try
+    match (List.find (fun t -> String.prefix ~pre:t str) tactics) with
+    | "xcf" -> `Xcf (vexpr_to_string vexpr)
+    | "xpullpure" -> `Xpullpure (vexpr_to_string vexpr)
+    | _ -> `Xcf (vexpr_to_string vexpr)
+  with
+  | Not_found -> assert false
+
+let parse_tactic t vexpr  =
+  match t with
+  | Sexplib.Sexp.List (_ :: _ :: _ :: t' :: _) ->
+    (
+      match t' with
+      | Sexplib.Sexp.List (t'' :: _) ->
+        (match t'' with
+        | Sexplib.Sexp.List (_ :: ((t3:: _))) ->
+          (match t3 with
+           | Sexplib.Sexp.List (_ :: (t4 :: _)) ->
+            (match t4 with
+            | Sexplib.Sexp.List (t5 :: _) ->
+              (match t5 with
+               | Sexplib.Sexp.List (_ :: _ :: (t6 :: _)) ->
+                (match t6 with
+                 | Sexplib.Sexp.List (_ :: (t7 :: _)) ->
+                   (match t7 with
+                      | Sexplib.Sexp.Atom str -> get_tactic str vexpr
+                      | _ -> assert false
+                   )
+                 | _ -> assert false
+                )
+               | _ -> assert false
+              )
+              | _ -> assert false
+              )
+            | _ -> assert false
+            )
+          | _ -> assert false
+          )
+        | _ -> assert false
+        )
+      | _ -> assert false
+  | _ -> assert false
+
+
+let parse_proof (asts: Vernacexpr.vernac_expr list) =
+  let program_id = ref 0 in
+  let steps = ref [] in
+
+  List.iter (fun vexpr ->
+      match vexpr with
+      | Vernacexpr.VernacExtend (ename, args) ->
+        let tactic_arg = List.nth args 2 in (* args = [selector; flag; tactic; default] *)
+        let t = Serlib.Ser_genarg.sexp_of_raw_generic_argument tactic_arg in
+        let step  = parse_tactic t vexpr in
+        print_endline @@ show_step step;
+        steps := step :: !steps;
+      | _ ->
+        steps := !steps;
+
+      program_id := !program_id + 1;
+  ) asts;
+  
+  print_endline @@ show_steps !steps;
+  ()
 
 let () =
-  match statement_0.expr with
-  | Vernacexpr.VernacLoad (_, _) |Vernacexpr.VernacReservedNotation (_, _)
-|Vernacexpr.VernacOpenCloseScope (_, _) |Vernacexpr.VernacDeclareScope _
-|Vernacexpr.VernacDelimiters (_, _) |Vernacexpr.VernacBindScope (_, _)
-|Vernacexpr.VernacNotation (_, _, _, _) |Vernacexpr.VernacNotationAddFormat
-(_, _, _) |Vernacexpr.VernacDeclareCustomEntry _ |Vernacexpr.VernacDefinition
-(_, _, _) |Vernacexpr.VernacStartTheoremProof (_, _)
-|Vernacexpr.VernacEndProof _ |Vernacexpr.VernacExactProof _
-|Vernacexpr.VernacAssumption (_, _, _) |Vernacexpr.VernacInductive (_, _)
-|Vernacexpr.VernacFixpoint (_, _) |Vernacexpr.VernacCoFixpoint (_, _)
-|Vernacexpr.VernacScheme _ |Vernacexpr.VernacCombinedScheme (_, _)
-|Vernacexpr.VernacUniverse _ |Vernacexpr.VernacConstraint _
-|Vernacexpr.VernacBeginSection _ |Vernacexpr.VernacEndSegment _
-|Vernacexpr.VernacRequire (_, _, _) |Vernacexpr.VernacImport (_, _, _)
-|Vernacexpr.VernacCanonical _ |Vernacexpr.VernacCoercion (_, _, _)
-|Vernacexpr.VernacIdentityCoercion (_, _, _)
-|Vernacexpr.VernacNameSectionHypSet (_, _) |Vernacexpr.VernacInstance
-(_, _, _, _, _) |Vernacexpr.VernacDeclareInstance (_, _, _, _)
-|Vernacexpr.VernacContext _ |Vernacexpr.VernacExistingInstance _
-|Vernacexpr.VernacExistingClass _ |Vernacexpr.VernacDeclareModule
-(_, _, _, _) |Vernacexpr.VernacDefineModule (_, _, _, _, _)
-|Vernacexpr.VernacDeclareModuleType (_, _, _, _) |Vernacexpr.VernacInclude _
-|Vernacexpr.VernacAddLoadPath _ |Vernacexpr.VernacRemoveLoadPath _
-|Vernacexpr.VernacAddMLPath _ |Vernacexpr.VernacDeclareMLModule _
-|Vernacexpr.VernacChdir _ |Vernacexpr.VernacResetName _
-|Vernacexpr.VernacResetInitial |Vernacexpr.VernacBack _
-|Vernacexpr.VernacCreateHintDb (_, _) |Vernacexpr.VernacRemoveHints (_, _)
-|Vernacexpr.VernacHints (_, _) |Vernacexpr.VernacSyntacticDefinition
-(_, _, _) |Vernacexpr.VernacArguments (_, _, _, _) |Vernacexpr.VernacReserve
-_ |Vernacexpr.VernacGeneralizable _ |Vernacexpr.VernacSetOpacity _
-|Vernacexpr.VernacSetStrategy _ 
-|Vernacexpr.VernacAddOption (_, _) |Vernacexpr.VernacRemoveOption (_, _)
-|Vernacexpr.VernacMemOption (_, _) |Vernacexpr.VernacPrintOption _
-|Vernacexpr.VernacCheckMayEval (_, _, _) |Vernacexpr.VernacGlobalCheck _
-|Vernacexpr.VernacDeclareReduction (_, _) |Vernacexpr.VernacPrint _
-|Vernacexpr.VernacSearch (_, _, _) |Vernacexpr.VernacLocate _
-|Vernacexpr.VernacRegister (_, _) |Vernacexpr.VernacPrimitive (_, _, _)
-|Vernacexpr.VernacComments _ |Vernacexpr.VernacAbort _
-|Vernacexpr.VernacAbortAll |Vernacexpr.VernacRestart |Vernacexpr.VernacUndo _
-|Vernacexpr.VernacUndoTo _ |Vernacexpr.VernacFocus _
-|Vernacexpr.VernacUnfocus |Vernacexpr.VernacUnfocused
-|Vernacexpr.VernacBullet _ |Vernacexpr.VernacSubproof _
-|Vernacexpr.VernacEndSubproof |Vernacexpr.VernacShow _
-|Vernacexpr.VernacCheckGuard |Vernacexpr.VernacProof (_, _)
-|Vernacexpr.VernacProofMode _ |Vernacexpr.VernacExtend (_, _) -> print_endline "hello world"
-|Vernacexpr.VernacSetOption (_, _, _) -> print_endline "Found set option"
+  let dec_str, rest = handle_decs asts in
+  print_endline dec_str;
+
+  let spec_str, rest' = handle_spec rest in
+  print_endline spec_str;
+
+  let mp (x: Vernacexpr.vernac_control_r CAst.t) = x.v.expr in
+  let rest'' = List.map mp rest' in
+  parse_proof rest'';
+  (* let lemma, rest'  List.hd @@ rest, List.tl @@ rest in *)
+  (* let spec, rest'' = parse_spec lemma in *)
+  ()
