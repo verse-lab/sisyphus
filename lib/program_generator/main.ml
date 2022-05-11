e928e6f8f1258ca22a3bdee6fa1cf4a1f483dd4b
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-05-11 06:55:55 +0100

    updated implemented proof generation up to xapp generation

--- lib/program_generator/main.ml
+++ lib/program_generator/main.ml
@@ -1,6 +1,9 @@
 [@@@warning "-33-26"]
 open Containers
 
+module StringMap = Map.Make(String)
+module StringSet = Set.Make(String)
+
 let old_program =
   IO.with_in "../../_build/default/resources/seq_to_array/seq_to_array_old.ml" IO.read_all
   |> Lang.Sanitizer.parse_str
@@ -8,7 +11,13 @@ let old_program =
 let new_program =
   IO.with_in "../../_build/default/resources/seq_to_array/seq_to_array_new.ml" IO.read_all
   |> Lang.Sanitizer.parse_str
-  
+
+let drop_last ls =
+  let rec loop acc = function
+    | [] | [_] -> List.rev acc
+    | h :: t -> loop (h :: acc) t in
+  loop [] ls
+
 let prelude = {coq|
 Set Implicit Arguments.
 
@@ -18,7 +27,29 @@ From TLC Require Import LibListZ.
 From Proofs Require Import Verify_seq_to_array_utils.
 From Proofs Require Import Seq_to_array_new_ml.
 |coq}
-  
+
+let tag ty =
+  match Constr.kind ty with
+  | Constr.Rel _ -> "Rel"
+  | Constr.Var _ -> "Var"
+  | Constr.Meta _ -> "Meta"
+  | Constr.Evar _ -> "Evar"
+  | Constr.Sort _ -> "Sort"
+  | Constr.Cast (_, _, _) -> "Cast"
+  | Constr.Prod (_, _, _) -> "Prod"
+  | Constr.Lambda (_, _, _) -> "Lambda"
+  | Constr.LetIn (_, _, _, _) -> "LetIn"
+  | Constr.App (_, _) -> "App"
+  | Constr.Const _ -> "Const"
+  | Constr.Ind _ -> "Ind"
+  | Constr.Construct _ -> "Construct"
+  | Constr.Case (_, _, _, _, _, _, _) -> "Case"
+  | Constr.Fix _ -> "Fix"
+  | Constr.CoFix _ -> "CoFix"
+  | Constr.Proj (_, _) -> "Proj"
+  | Constr.Int _ -> "Int"
+  | Constr.Float _ -> "Float"
+  | Constr.Array (_, _, _, _)  -> "Array"  
 
 type coq_ctx = (module Coq.Proof.PROOF)
 let add (module Ctx: Coq.Proof.PROOF) txt =
@@ -72,6 +103,24 @@ let search ?at ctx query =
   );
   Some !acc
 
+let drop_implicits fn params =
+  let implicits = Impargs.implicits_of_global (Names.GlobRef.ConstRef fn) in
+  List.rev implicits
+  |> List.concat_map Fun.(List.rev % snd)
+  |> List.filter_map (Option.map (fun ((_, id,_), _, _) -> id))
+  |> List.fold_left (fun ls i -> List.remove_at_idx (i - 1) ls) params
+
+let coqobj_to_string v =
+  Format.to_string Pp.pp_with @@ Serapi.Serapi_protocol.gen_pp_obj Environ.empty_env Evd.empty v
+
+let typeof ?at (module Ctx: Coq.Proof.PROOF) name =
+  Ctx.query ?at Serapi.Serapi_protocol.(TypeOf name)
+  |> Option.map (List.map coqobj_to_string)
+  |> Option.map (String.concat "; ")
+  |> Option.get_or ~default:"NONE"
+  |> (fun s -> "[" ^ s ^ "]")
+  |> print_endline
+
 let find_spec ?at ctx const =
   search ?at ctx [
     true, Search.(GlobSearchLiteral (GlobSearchString "spec"));
@@ -83,6 +132,20 @@ let find_spec ?at ctx const =
   | Some [] -> failwith "failure finding specification for function application: could not find an appropriate specification"
   | Some _ -> failwith "failure finding specification for function application: ambiguity - more than one valid specification found"
   | None -> failwith "internal Coq error: unable to search in context"
+
+let is_pure (prog: [ `Lambda of Lang.Expr.typed_param list * Lang.Expr.t Lang.Program.stmt ]) =
+  let rec loop (stmt: Lang.Expr.t Lang.Program.stmt) =
+    match stmt with
+    | `Match (_, cases) ->
+      List.for_all (fun (_, _, body) -> loop body) cases
+    | `LetLambda (_, `Lambda (_, body), rest) ->
+      loop body && loop rest
+    | `EmptyArray -> true
+    | `Write _ -> false
+    | `Value _ -> true
+    | `LetExp (_, _, rest) -> loop rest in
+  match prog with
+  | `Lambda (_, body) -> loop body
 
 let generate (alignment: Dynamic.Matcher.t) (ctx: coq_ctx) (prog: Lang.Expr.t Lang.Program.t) =
   (* TODO: add spec for function.... *)
@@ -211,13 +274,32 @@ Proof using.
       |> fst
     with
       Failure _ -> failwith ("extract_f_app failed because unsupported context: " ^ (to_string pre)) in
+
+  let extract_spec pre =
+    let rec loop acc pre = 
+      if Constr.isProd pre
+      then
+        let ({Context.binder_name; _}, ty, pre)  = Constr.destProd pre in
+        loop ((binder_name, ty) :: acc) pre
+      else List.rev acc, pre in
+    loop [] pre in
+  let extract_spec pre =
+    let rec split acc ls =
+      match ls with
+      | [] -> (List.rev acc,[])
+      | ((name, _) as h) :: t when Names.Name.is_anonymous name ->
+        (List.rev acc, h::t)
+      | h :: t -> split (h :: acc) t in
+    let params, body = extract_spec pre in
+    let params, invariants = split [] params in
+    (params, invariants, body) in
   begin match pre with
   | `Empty -> ()
   | `NonEmpty ls ->
     let no_pure = List.count (function `Pure _ -> true | _ -> false) ls in
     intro_pure no_pure;
   end;
-  let rec loop (body: Lang.Expr.t Lang.Program.stmt) =
+  let rec loop env (body: Lang.Expr.t Lang.Program.stmt) =
     print_endline @@ Format.sprintf "remaining program is: %a" (Lang.Program.pp_stmt_line Lang.Expr.print) body;
     debug_print_current_goal ();
     (match body with
@@ -225,23 +307,87 @@ Proof using.
        let fname = fresh ~base:name () in
        let h_fname = fresh ~base:("H" ^ name) () in
        add_and_exec ctx (Format.sprintf "xletopaque %s %s." fname h_fname);
-       loop rest
-     | `LetExp _ ->
-       let (_, post) = extract_cfml_goal () in
-       let f_app = extract_x_app_fun post in
-       let (refr, ty) = find_spec ctx f_app in
-       print_endline @@ Printf.sprintf "search result: %s (%s)" (Names.Constant.to_string refr) (to_string ty);
-       (* Search.generic_search env (fun gr _ _ typ ) *)
-       (* print_endline @@ Printf.sprintf "got %s (is %b)"  (to_string const) (Constr.isConst const); *)
+       let env = StringMap.add name body env in
+       loop env rest
+     | `LetExp (pat, body, rest) ->
+       begin match body with
+       (* pure function application *)
+       | `App (_, prog_args)
+         when List.exists (function
+           |`Var v -> StringMap.find_opt v env |> Option.exists is_pure
+           | _ -> false
+           ) prog_args ->
+         (* work out the name of function being called and the spec for it *)
+         let (f_name, raw_spec) =
+           (* extract the proof script name for the function being called *)
+           let (_, post) = extract_cfml_goal () in
+           let f_app = extract_x_app_fun post in
+           (* use Coq's searching functionality to work out the spec for the function *)
+           find_spec ctx f_app in
+         let (params, invariant, spec) = extract_spec raw_spec in
 
-       failwith "don't know how to handle let bindings"
+         (* work out the parameters to instantiate *)
+         let evar_params =
+           params
+           (* drop implicits from parameters *)
+           |> drop_implicits f_name
+           (* drop concrete arguments *)
+           |> List.drop (List.length prog_args)
+           (* last parameter is the concretisation of the pure function *)
+           |> drop_last in
+
+         (* instantiate evars, and collect variables to clear at end *)
+         let clear_vars =
+           List.fold_left (fun to_clear (name, _) ->
+             let name = Format.to_string Pp.pp_with @@ Names.Name.print name in
+             let ty = fresh ~base:("ty_" ^ name ) () in
+             add_and_exec ctx @@ Printf.sprintf "evar (%s: Type)." ty;
+             let name = fresh ~base:name () in
+             add_and_exec ctx @@ Printf.sprintf "evar (%s: %s)." name ty;
+             to_clear
+             |> StringSet.add ty
+             |> StringSet.add name
+           ) StringSet.empty evar_params in
+
+         let fn_body =
+           List.find_map (function `Var v -> StringMap.find_opt v env |> Option.flat_map (Option.if_ is_pure) | _ -> None)
+             prog_args
+           |> Option.get_exn_or "invalid assumptions" in
+
+         print_endline @@ "command is:\n" ^  Printf.sprintf
+                               "xapp (%s %s %s %s)."
+                               (Names.Constant.to_string f_name)
+                               (List.map (Format.to_string Lang.Expr.pp) prog_args
+                                |> String.concat " ")
+                               (List.map (fun (name, _) -> "?" ^ Format.to_string Pp.pp_with (Names.Name.print name))
+                                  evar_params
+                                |> String.concat " ")
+                               (Format.to_string (Lang.Program.pp_lambda Lang.Expr.print_simple) fn_body);
+
+
+
+         List.iter (fun (name, ty) ->
+           print_endline @@ Format.sprintf "param %a (%b): %s" Pp.pp_with
+                              (Names.Name.print name)
+                              (Names.Name.is_anonymous name)
+                              (to_string ty)
+         ) evar_params;
+
+         (* Search.generic_search env (fun gr _ _ typ ) *)
+         (* print_endline @@ Printf.sprintf "got %s (is %b)"  (to_string const) (Constr.isConst const); *)
+
+         failwith "don't know how to handle let bindings"
+       | _ ->
+         failwith ("TODO: implement handling of let _ = " ^ Format.to_string Lang.Expr.pp body ^ " expressions")
+       end
      | `Match _ -> failwith "don't know how to handle matches"
      | `EmptyArray -> failwith "don't know how to handle empty arrays"
      | `Write _ -> failwith "don't know how to handle write"
      | `Value _ -> failwith "don't know how to handle value"
     )
   in
-  ignore @@ loop prog.body;
+  let env = StringMap.empty in
+  ignore @@ loop env prog.body;
   ()
 
 
