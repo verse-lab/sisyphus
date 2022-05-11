58f961e1db5b531ed0e884eb2dcbf2226ad1bc9e
mkeoliya <e0407889@u.nus.edu>
2022-05-11 14:56:55 +0800

    Add destructs for VernacExpr

--- lib/proof_parser/main.ml
+++ lib/proof_parser/main.ml
@@ -4,7 +4,6 @@ let proof_str = IO.with_in "../../resources/seq_to_array/Verify_seq_to_array_old
 
 let () = print_endline @@ "Hello world" ^ proof_str
 
-
 let print_tag = function
   | Vernacexpr.VernacLoad (_, _) -> "VernacLoad"
   |Vernacexpr.VernacReservedNotation (_, _)
@@ -136,6 +135,8 @@ let print_tag = function
   |Vernacexpr.VernacSetOption (_, _, _) -> "VernacSetOption"
 
 
+=======
+>>>>>>> parent of 28c80d5... added print_tag function
 let asts =
   let module Ctx =
     Coq.Proof.Make(struct
@@ -154,11 +155,11 @@ let asts =
   (* TODO: Candidate for function*)
   Iter.int_range_by ~step:(-1) start 0
   |> Iter.filter_map (fun at ->
-    Ctx.query ~at Serapi.Serapi_protocol.Ast 
+   Ctx.query ~at Serapi.Serapi_protocol.Ast 
   )
   |> Iter.flat_map_l Fun.id
-  |> Iter.filter_map (function Serapi.Serapi_protocol.CoqAst v ->
-
+  |> Iter.filter_map (function Serapi.Serapi_protocol.CoqAst v as ast ->
+    Format.printf "%7s, %a\n" (print_tag v.v.expr) Pp.pp_with (Serapi.Serapi_protocol.gen_pp_obj Environ.empty_env Evd.empty ast);
     Some v.v
                              | _ -> None
   )
@@ -170,47 +171,47 @@ let statement_0 = List.hd asts
 let () =
   match statement_0.expr with
   | Vernacexpr.VernacLoad (_, _) |Vernacexpr.VernacReservedNotation (_, _)
-  |Vernacexpr.VernacOpenCloseScope (_, _) |Vernacexpr.VernacDeclareScope _
-  |Vernacexpr.VernacDelimiters (_, _) |Vernacexpr.VernacBindScope (_, _)
-  |Vernacexpr.VernacNotation (_, _, _, _) |Vernacexpr.VernacNotationAddFormat
-                                             (_, _, _) |Vernacexpr.VernacDeclareCustomEntry _ |Vernacexpr.VernacDefinition
-                                                                                                 (_, _, _) |Vernacexpr.VernacStartTheoremProof (_, _)
-  |Vernacexpr.VernacEndProof _ |Vernacexpr.VernacExactProof _
-  |Vernacexpr.VernacAssumption (_, _, _) |Vernacexpr.VernacInductive (_, _)
-  |Vernacexpr.VernacFixpoint (_, _) |Vernacexpr.VernacCoFixpoint (_, _)
-  |Vernacexpr.VernacScheme _ |Vernacexpr.VernacCombinedScheme (_, _)
-  |Vernacexpr.VernacUniverse _ |Vernacexpr.VernacConstraint _
-  |Vernacexpr.VernacBeginSection _ |Vernacexpr.VernacEndSegment _
-  |Vernacexpr.VernacRequire (_, _, _) |Vernacexpr.VernacImport (_, _, _)
-  |Vernacexpr.VernacCanonical _ |Vernacexpr.VernacCoercion (_, _, _)
-  |Vernacexpr.VernacIdentityCoercion (_, _, _)
-  |Vernacexpr.VernacNameSectionHypSet (_, _) |Vernacexpr.VernacInstance
-                                                (_, _, _, _, _) |Vernacexpr.VernacDeclareInstance (_, _, _, _)
-  |Vernacexpr.VernacContext _ |Vernacexpr.VernacExistingInstance _
-  |Vernacexpr.VernacExistingClass _ |Vernacexpr.VernacDeclareModule
-                                       (_, _, _, _) |Vernacexpr.VernacDefineModule (_, _, _, _, _)
-  |Vernacexpr.VernacDeclareModuleType (_, _, _, _) |Vernacexpr.VernacInclude _
-  |Vernacexpr.VernacAddLoadPath _ |Vernacexpr.VernacRemoveLoadPath _
-  |Vernacexpr.VernacAddMLPath _ |Vernacexpr.VernacDeclareMLModule _
-  |Vernacexpr.VernacChdir _ |Vernacexpr.VernacResetName _
-  |Vernacexpr.VernacResetInitial |Vernacexpr.VernacBack _
-  |Vernacexpr.VernacCreateHintDb (_, _) |Vernacexpr.VernacRemoveHints (_, _)
-  |Vernacexpr.VernacHints (_, _) |Vernacexpr.VernacSyntacticDefinition
-                                    (_, _, _) |Vernacexpr.VernacArguments (_, _, _, _) |Vernacexpr.VernacReserve
-                                                                                          _ |Vernacexpr.VernacGeneralizable _ |Vernacexpr.VernacSetOpacity _
-  |Vernacexpr.VernacSetStrategy _ 
-  |Vernacexpr.VernacAddOption (_, _) |Vernacexpr.VernacRemoveOption (_, _)
-  |Vernacexpr.VernacMemOption (_, _) |Vernacexpr.VernacPrintOption _
-  |Vernacexpr.VernacCheckMayEval (_, _, _) |Vernacexpr.VernacGlobalCheck _
-  |Vernacexpr.VernacDeclareReduction (_, _) |Vernacexpr.VernacPrint _
-  |Vernacexpr.VernacSearch (_, _, _) |Vernacexpr.VernacLocate _
-  |Vernacexpr.VernacRegister (_, _) |Vernacexpr.VernacPrimitive (_, _, _)
-  |Vernacexpr.VernacComments _ |Vernacexpr.VernacAbort _
-  |Vernacexpr.VernacAbortAll |Vernacexpr.VernacRestart |Vernacexpr.VernacUndo _
-  |Vernacexpr.VernacUndoTo _ |Vernacexpr.VernacFocus _
-  |Vernacexpr.VernacUnfocus |Vernacexpr.VernacUnfocused
-  |Vernacexpr.VernacBullet _ |Vernacexpr.VernacSubproof _
-  |Vernacexpr.VernacEndSubproof |Vernacexpr.VernacShow _
-  |Vernacexpr.VernacCheckGuard |Vernacexpr.VernacProof (_, _)
-  |Vernacexpr.VernacProofMode _ |Vernacexpr.VernacExtend (_, _) -> print_endline "hello world"
-  |Vernacexpr.VernacSetOption (_, _, _) -> print_endline "Found set option"
+|Vernacexpr.VernacOpenCloseScope (_, _) |Vernacexpr.VernacDeclareScope _
+|Vernacexpr.VernacDelimiters (_, _) |Vernacexpr.VernacBindScope (_, _)
+|Vernacexpr.VernacNotation (_, _, _, _) |Vernacexpr.VernacNotationAddFormat
+(_, _, _) |Vernacexpr.VernacDeclareCustomEntry _ |Vernacexpr.VernacDefinition
+(_, _, _) |Vernacexpr.VernacStartTheoremProof (_, _)
+|Vernacexpr.VernacEndProof _ |Vernacexpr.VernacExactProof _
+|Vernacexpr.VernacAssumption (_, _, _) |Vernacexpr.VernacInductive (_, _)
+|Vernacexpr.VernacFixpoint (_, _) |Vernacexpr.VernacCoFixpoint (_, _)
+|Vernacexpr.VernacScheme _ |Vernacexpr.VernacCombinedScheme (_, _)
+|Vernacexpr.VernacUniverse _ |Vernacexpr.VernacConstraint _
+|Vernacexpr.VernacBeginSection _ |Vernacexpr.VernacEndSegment _
+|Vernacexpr.VernacRequire (_, _, _) |Vernacexpr.VernacImport (_, _, _)
+|Vernacexpr.VernacCanonical _ |Vernacexpr.VernacCoercion (_, _, _)
+|Vernacexpr.VernacIdentityCoercion (_, _, _)
+|Vernacexpr.VernacNameSectionHypSet (_, _) |Vernacexpr.VernacInstance
+(_, _, _, _, _) |Vernacexpr.VernacDeclareInstance (_, _, _, _)
+|Vernacexpr.VernacContext _ |Vernacexpr.VernacExistingInstance _
+|Vernacexpr.VernacExistingClass _ |Vernacexpr.VernacDeclareModule
+(_, _, _, _) |Vernacexpr.VernacDefineModule (_, _, _, _, _)
+|Vernacexpr.VernacDeclareModuleType (_, _, _, _) |Vernacexpr.VernacInclude _
+|Vernacexpr.VernacAddLoadPath _ |Vernacexpr.VernacRemoveLoadPath _
+|Vernacexpr.VernacAddMLPath _ |Vernacexpr.VernacDeclareMLModule _
+|Vernacexpr.VernacChdir _ |Vernacexpr.VernacResetName _
+|Vernacexpr.VernacResetInitial |Vernacexpr.VernacBack _
+|Vernacexpr.VernacCreateHintDb (_, _) |Vernacexpr.VernacRemoveHints (_, _)
+|Vernacexpr.VernacHints (_, _) |Vernacexpr.VernacSyntacticDefinition
+(_, _, _) |Vernacexpr.VernacArguments (_, _, _, _) |Vernacexpr.VernacReserve
+_ |Vernacexpr.VernacGeneralizable _ |Vernacexpr.VernacSetOpacity _
+|Vernacexpr.VernacSetStrategy _ 
+|Vernacexpr.VernacAddOption (_, _) |Vernacexpr.VernacRemoveOption (_, _)
+|Vernacexpr.VernacMemOption (_, _) |Vernacexpr.VernacPrintOption _
+|Vernacexpr.VernacCheckMayEval (_, _, _) |Vernacexpr.VernacGlobalCheck _
+|Vernacexpr.VernacDeclareReduction (_, _) |Vernacexpr.VernacPrint _
+|Vernacexpr.VernacSearch (_, _, _) |Vernacexpr.VernacLocate _
+|Vernacexpr.VernacRegister (_, _) |Vernacexpr.VernacPrimitive (_, _, _)
+|Vernacexpr.VernacComments _ |Vernacexpr.VernacAbort _
+|Vernacexpr.VernacAbortAll |Vernacexpr.VernacRestart |Vernacexpr.VernacUndo _
+|Vernacexpr.VernacUndoTo _ |Vernacexpr.VernacFocus _
+|Vernacexpr.VernacUnfocus |Vernacexpr.VernacUnfocused
+|Vernacexpr.VernacBullet _ |Vernacexpr.VernacSubproof _
+|Vernacexpr.VernacEndSubproof |Vernacexpr.VernacShow _
+|Vernacexpr.VernacCheckGuard |Vernacexpr.VernacProof (_, _)
+|Vernacexpr.VernacProofMode _ |Vernacexpr.VernacExtend (_, _) -> print_endline "hello world"
+|Vernacexpr.VernacSetOption (_, _, _) -> print_endline "Found set option"
