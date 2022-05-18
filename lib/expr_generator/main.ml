245e287fb4fa155e98cddc157d0e403bdbfad14c
mkeoliya <e0407889@u.nus.edu>
2022-05-18 14:12:49 +0800

    Add driving code for constant/function collection, use custom type to represent constants & functions

--- lib/expr_generator/main.ml
+++ lib/expr_generator/main.ml
@@ -10,43 +10,94 @@ type pat =
   ]
 [@@deriving eq, ord]
 
+type expr = [
+  | `Int of int
+  | `Func of string
+]
+
+let show_expr = function
+  | `Int i -> string_of_int i
+  | `Func fname -> fname
+
 (* assumption: no strings used*)
-let collect_expr = function
-  | `App (_, _) | `Int _ as e  -> Some e
-  | _ -> None
+let rec collect_expr : Lang.Expr.t -> expr list = function
+  | `Int _ as e -> [e]
+  | `App (fname, exps)  ->
+    [`Func fname] @ List.flat_map collect_expr exps
+  | `Constructor (_, es) ->
+    List.flat_map collect_expr es
+  | _ ->
+    []
 
 let collect = let open Proof_spec.Heap in function
-  | `Expr e ->
-    [collect_expr e]
-  | `Spec (_, asn) ->
-    let collect_heaplet = function
-      | PointsTo (_, e) -> collect_expr e in 
-   List.filter_map Fun.id (fold (fun acc x -> collect_heaplet x :: acc ) [] asn.sigma)
-  | `Hole -> failwith "holes not supported"
+    | `Expr e -> collect_expr e
+    | `Spec (_, asn) ->
+      let collect_heaplet = function
+        | Heaplet.PointsTo (_, e) ->  collect_expr e in
+       List.flat_map collect_heaplet (Assertion.sigma asn)
+    | `Hole -> failwith "holes not supported"
 
-let collect_in = function
+let collect_in =
+  function
   | `Xapp (_, _, args) ->
-    List.flat_map collect args
-  | _ -> []
+    List.iter (fun x -> print_endline @@ Proof_spec.Script.show_spec_arg x) args;
+    let ls = List.flat_map collect args in
+    print_endline @@ string_of_int @@ List.length ls;
+    ls
+  | _ ->
+    []
 
-let collect_constants from_id to_id steps =
+let collect_constants from_id to_id (steps: Proof_spec.Script.step list)  =
   let rec cc_aux steps =
     match steps with
     | [] -> []
     | h :: t ->
       let curr = match h with
-      | `Xapp (id, _, _, _) :: _
-      | `Case (id, _, _, _) :: _
-      | `Xvalemptyarr (id, _, _) :: _
-      | `Xalloc (id, _, _) :: _
-      | `Xletaopaque (id, _, _) :: _
-      | `Xvals (id, _, _) :: _ -> id
-      | _ -> -1 in
+        | `Xapp (id, _, _) | `Case (id, _, _, _) | `Xvalemptyarr (id, _)  | `Xalloc (id, _) | `Xletopaque (id, _)  | `Xvals (id, _) -> id
+        | _ -> -1 (* assume: from_id >= 0 *)
+      in
+
+      print_endline @@ string_of_int curr;
 
       if curr >= from_id && curr <= to_id
-      then (collect_in h) @ cc_aux t
+      then
+        (collect_in h) @ cc_aux t
       else cc_aux t
   in
   cc_aux steps
+
+
+let () =
+  let proof_str = IO.with_in "../../resources/seq_to_array/Verify_seq_to_array_old.v" IO.read_all in
+  let dir = "../../_build/default/resources/seq_to_array" in
+  let module Ctx =
+    Coq.Proof.Make(struct
+      let verbose = false
+      let libs = [
+        Coq.Coqlib.make
+          ~path:(Fpath.of_string dir |> Result.get_exn)
+          "Proofs"
+      ]
+    end) in
+
+  let parsed_script = Proof_parser.Parser.parse (module Ctx) proof_str in
+  Proof_spec.Script.pp_parsed_script parsed_script;
+
+  let steps = parsed_script.proof in
+  let case = List.nth steps 5 in
+
+  let steps = match [@warning "-8"] case with
+    | `Case (_, _, _, (_ :: (_, steps) :: _)) -> steps
+  in
+
+  let xapp = List.nth steps 4 in
+  print_endline @@ Proof_spec.Script.show_step xapp;
+
+  let cc  = collect_constants 0 10 [xapp] in
+  List.iter (fun x -> print_endline @@ show_expr x) cc;
+  ()
+
+
+
 
 
