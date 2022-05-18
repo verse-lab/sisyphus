5b1d14d6ab277f6f2c780066531cbe8610144a68
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-05-18 07:07:56 +0100

    extracting of env + assumptions

--- lib/proof_generator/specification.ml
+++ lib/proof_generator/specification.ml
@@ -1,6 +1,12 @@
 open Containers
 module StringMap = Map.Make(String)
 
+let () =
+  Printexc.register_printer (function
+      Failure msg -> Some msg
+    | _ -> None
+  )
+
 type expr = Lang.Expr.t
 type holy_expr = expr -> expr
 type ty = Lang.Type.t
@@ -42,6 +48,7 @@ type vc = {
 } [@@deriving show]
 
 type verification_condition = {
+  env: (string * ty) list;
   initial: initial_vc;
   conditions: vc list;
 } [@@deriving show]
@@ -68,35 +75,35 @@ let is_const_eq str fn =
 
 let is_constr_cons fn =
   is_constr_eq "Coq.Init.Datatypes.list" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 2
+  && (Constr.destConstruct fn |> fst |> snd) = 2
 
 let is_constr_nil fn =
   is_constr_eq "Coq.Init.Datatypes.list" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 1
+  && (Constr.destConstruct fn |> fst |> snd) = 1
 
 let is_constr_z0 fn =
   is_constr_eq "Coq.Numbers.BinNums.Z" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 1
+  && (Constr.destConstruct fn |> fst |> snd) = 1
 
 let is_constr_zpos fn =
   is_constr_eq "Coq.Numbers.BinNums.Z" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 2
+  && (Constr.destConstruct fn |> fst |> snd) = 2
 
 let is_constr_zneg fn =
   is_constr_eq "Coq.Numbers.BinNums.Z" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 3
+  && (Constr.destConstruct fn |> fst |> snd) = 3
 
 let is_constr_pos_x0 fn =
   is_constr_eq "Coq.Numbers.BinNums.positive" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 2
+  && (Constr.destConstruct fn |> fst |> snd) = 2
 
 let is_constr_pos_x1 fn =
   is_constr_eq "Coq.Numbers.BinNums.positive" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 1
+  && (Constr.destConstruct fn |> fst |> snd) = 1
 
 let is_constr_pos_xh fn =
   is_constr_eq "Coq.Numbers.BinNums.positive" fn
-    && (Constr.destConstruct fn |> fst |> snd) = 3
+  && (Constr.destConstruct fn |> fst |> snd) = 3
 
 
 let rec extract_typ ?rel (c: Constr.t) : Lang.Type.t =
@@ -107,7 +114,9 @@ let rec extract_typ ?rel (c: Constr.t) : Lang.Type.t =
       | _ -> Format.ksprintf ~f:failwith "found unknown type %s" (Names.MutInd.to_string name)
     end
   | Constr.App (fname, [|ty|]), _ when is_ind_eq "Coq.Init.Datatypes.list" fname -> 
-      List (extract_typ ?rel ty)
+    List (extract_typ ?rel ty)
+  | Constr.App (fname, [|ty|]), _ when is_const_eq "CFML.WPBuiltin.array" fname -> 
+    Array (extract_typ ?rel ty)
   | Constr.App (fname, args), _ when is_ind_eq "Coq.Init.Datatypes.prod" fname ->
     Product (Array.to_iter args |> Iter.map (extract_typ ?rel) |> Iter.to_list)
   | Constr.Var name, _ -> Var (Names.Id.to_string name)
@@ -117,6 +126,14 @@ let rec extract_typ ?rel (c: Constr.t) : Lang.Type.t =
       (Proof_debug.constr_to_string c)
       (Proof_debug.tag c)
       (Proof_debug.constr_to_string_pretty c)
+
+let extract_typ_opt ?rel c =
+  try
+    Some (extract_typ ?rel c)
+  with
+    Failure msg ->
+    Format.printf "suppressing error %s@." msg;
+    None
 
 let extract_const_int (c: Constr.t) : Lang.Expr.t =
   let rec extract_int c =
@@ -128,8 +145,8 @@ let extract_const_int (c: Constr.t) : Lang.Expr.t =
     | _ when is_constr_pos_xh c ->
       1
     | _ ->
-    Format.ksprintf ~f:failwith "found unhandled Coq term (%s) that could not be converted to a constant int"
-      (Proof_debug.constr_to_string c) in
+      Format.ksprintf ~f:failwith "found unhandled Coq term (%s) that could not be converted to a constant int"
+        (Proof_debug.constr_to_string c) in
   match Constr.kind c with
   | Constr.App (const, [|inner|]) when is_constr_zpos const ->
     `Int (extract_int inner)
@@ -199,27 +216,67 @@ let rec extract_expr ctx (c: Constr.t) : Lang.Expr.t =
   | _ ->
     Format.ksprintf ~f:failwith "found unhandled Coq term (%s)[%s] in %s that could not be converted to a expr"
       (Proof_debug.constr_to_string c) (Proof_debug.tag c) (Proof_debug.constr_to_string_pretty c)
-  
+
+
+let extract_env (t: Proof_context.t) =
+  List.filter_map (fun (name, o_vl, vl) ->
+    let name = (List.map Names.Id.to_string name |> String.concat ".") in
+    begin match Constr.kind vl with
+    | Constr.Sort _ -> Some (name, `Type) (* represents (A: Type) *)
+    | Constr.App (fn, [| ty; l; r |]) when is_coq_eq fn -> None
+    | Constr.Const _ when is_const_eq "CFML.Semantics.loc" vl ->
+      Some (name, `Val Lang.Type.Loc)
+    | Constr.Const _ when is_const_eq "CFML.WPBuiltin.func" vl ->
+      Some (name, `Val Lang.Type.Func)
+    | Constr.App (fn, _)
+      when is_const_eq "CFML.WPLifted.Wpgen_body" fn
+        || is_const_eq "CFML.WPLifted.Wpgen_negpat" fn ->
+      None                    (* specifications *)
+    | Constr.Ind ((ty_name, _), _) ->
+      Some (name, `Val (Lang.Type.Var (Names.MutInd.to_string ty_name)))
+    | Constr.Var _              (* init: A *)
+    | Constr.App (_, _) ->
+      Option.map (fun vl -> (name, `Val (vl))) (extract_typ_opt vl)
+    (* list A, and eq, and others *) 
+    | Constr.Const _
+    | _ ->
+      let c= vl in
+      Format.ksprintf ~f:failwith "found unhandled Coq term (%s)[%s] in (%s: %s) that could not be converted to a binding"
+        (Proof_debug.constr_to_string c) (Proof_debug.tag c) name (Proof_debug.constr_to_string_pretty c)
+    end
+  ) @@ List.rev (Proof_context.current_goal t).hyp
+  |> List.partition_filter_map (function
+    | (name, `Type) -> `Left name
+    | (name, `Val ty) -> `Right (name, ty)
+  )
+
+
+let extract_assumptions t =
+  List.filter_map (fun (name, o_vl, vl) ->
+    let _name = (List.map Names.Id.to_string name |> String.concat ".") in
+    begin match Constr.kind vl with
+    | Constr.Sort _ -> None     (* represents (A: Type) *)
+    | Constr.App (fn, [| ty; l; r |]) when is_coq_eq fn ->
+      let ty = extract_typ ty in
+      let l = extract_expr t l in
+      let r = extract_expr t r in
+      Some (ty, l, r)
+    | Constr.App (fn, args) -> (* list A, and eq, and others *) None
+    | Constr.Ind _              (* credits? *)
+    | Constr.Var _              (* init: A *)
+    | Constr.Const _ -> None
+    | _ -> None
+    end
+  ) @@ List.rev (Proof_context.current_goal t).hyp
 
 let build_verification_condition (t: Proof_context.t) : verification_condition =
-  let assumptions = 
-    List.filter_map (fun (name, o_vl, vl) ->
-      let _name = (List.map Names.Id.to_string name |> String.concat ".") in
-      begin match Constr.kind vl with
-      | Constr.Sort _ -> None     (* represents (A: Type) *)
-      | Constr.App (fn, [| ty; l; r |]) when is_coq_eq fn ->
-        let ty = extract_typ ty in
-        let l = extract_expr t l in
-        let r = extract_expr t r in
-        Some (ty, l, r)
-      | Constr.App (fn, args) -> (* list A, and eq, and others *) None
-      | Constr.Ind _              (* credits? *)
-      | Constr.Var _              (* init: A *)
-      | Constr.Const _ -> None
-      | _ -> None
-      end
-    ) (Proof_context.current_goal t).hyp in
-
+  let poly_vars, env = extract_env t in
+  let assumptions = extract_assumptions t in
+  List.iter (function
+    | (name, ty) ->
+      Format.printf "%s: %s@."
+      name ( Printer.show_ty ty )
+  ) env;
   List.iter (fun (ty, l, r) ->
     Format.printf "%s = %s (%s)@."
       Lang.Expr.(show l)
