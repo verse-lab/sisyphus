631747d278c9d31219c08e976e5d40bad05f0254
mkeoliya <e0407889@u.nus.edu>
2022-05-27 13:54:31 +0800

    Remove redundant patterns using PatternSet instead of lists

--- lib/expr_generator/patgen.ml
+++ lib/expr_generator/patgen.ml
@@ -1,9 +1,8 @@
 open Containers
 module PS = Proof_spec
 
-
 module PatternSet = Set.Make (struct
-    type t = Expgen.pat [@@deriving eq, show, ord]
+    type t = Expgen.tag_pat [@@deriving eq, ord, show]
   end)
 
 let construct_pat_vars fname input_types =
@@ -24,45 +23,49 @@ let gen_at_expr_no_recur types pat_var = function
     [pat; pat_var]
   | _ -> [pat_var]
 
-let rec gen_at_apply env fname args : Expgen.tag_pat list =
+let rec gen_at_apply env ps fname args =
   let input_types, ret_type = env fname in
-  let pat_vars = construct_pat_vars fname input_types in 
+  let pat_vars = construct_pat_vars fname input_types in
 
   let arg_pat_vars = List.combine args pat_vars in
-  let tag_pats =
+  let tag_pats : (Expgen.tag_pat list) =
     List.map (fun (arg, pat_var) -> gen_at_expr_no_recur env pat_var arg) arg_pat_vars
     |> List.cartesian_product
     |> List.map (fun (arg_pats) -> ret_type, `App (fname, arg_pats))
   in
 
   (* recurse on args*)
-  tag_pats @ List.flat_map (fun x -> gen_at_expr env x) args
+  let ps = PatternSet.add_list ps tag_pats in
+  List.fold_left (gen_at_expr env) ps args
 
-and gen_at_expr env = function
+and gen_at_expr env ps = function
   | `App (fname, args) ->
-    gen_at_apply env fname args
+    gen_at_apply env ps fname args
   | `Constructor (_, es) ->
-    List.flat_map (fun x -> gen_at_expr env x) es
-  | _ -> []
+    List.fold_left (gen_at_expr env) ps es
+  | _ -> ps
 
 
-let gen_at_spec_arg types = let open Proof_spec.Heap in function
-    | `Expr e -> []
+let gen_at_spec_arg env ps = let open Proof_spec.Heap in function
+    | `Expr e -> ps
     | `Spec (_, asn) ->
-      let gen_at_heaplet = function
-        | Heaplet.PointsTo (_, e) -> gen_at_expr types e in
-      List.flat_map gen_at_heaplet (Assertion.sigma asn)
+      let gen_at_heaplet ps = function
+        | Heaplet.PointsTo (_, e) -> gen_at_expr env ps e in
+      List.fold_left gen_at_heaplet ps (Assertion.sigma asn)
     | `Hole -> failwith "holes not supported"
 
 (* generate patterns from ALL possible steps, regardless of program ID *)
 let gen_pats steps ~from_id ~to_id ~env : Expgen.tag_pat list =
-  let gen_pats_at_step env acc = function
+  let gen_pats_at_step env ps = function
     | `Xapp (_, _, args) ->
-      acc @ List.flat_map (fun arg -> gen_at_spec_arg env arg) args
-    | _ -> acc
+      List.fold_left (gen_at_spec_arg env) ps args
+    | _ -> ps
   in
 
-  PS.Script.fold_proof_script ~start:from_id ~stop:to_id (gen_pats_at_step env) [] steps
+  PS.Script.fold_proof_script ~start:from_id ~stop:to_id
+    (gen_pats_at_step env) PatternSet.empty steps
+  |> PatternSet.to_list 
+
 
 let get_pat_type_map (env: string -> Expgen.func_type) (pats: Expgen.tag_pat list) =
   let add_type type_map = function
