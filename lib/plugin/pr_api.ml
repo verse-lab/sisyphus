0f49efeeed7440dd71b1f890809ebc601e3be01d
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-05-31 10:50:33 +0100

    forgive me sensei

--- lib/plugin/pr_api.ml
+++ lib/plugin/pr_api.ml
@@ -1,15 +1,24 @@
 
-let callC () = 10
+let access cb =
+  match Global.body_of_constant Library.indirect_accessor cb with
+  | Some (e, _, _) -> e
+  | None ->
+    CErrors.user_err (Pp.str "term has no value")
 
-let message = "Hello world!"
 
-module PV = Proofview
+(*
+   Opaqueproof
+ *)
 
-let msg_in_tactic str : unit PV.tactic =
-  PV.tclLIFT (PV.NonLogical.make (fun () ->
-      Feedback.msg_warning (Pp.str str)))
+let reduce e =
+  match Constr.kind e with
+  | Constr.Const (name, _) -> access name
+  | _ -> e
 
-let printHello : unit PV.tactic =
-  let open PV.Notations in
-  msg_in_tactic "hello" >>= fun () ->
-  Tacticals.tclIDTAC
+let print_reduced (e: Constrexpr.constr_expr_r CAst.t) =
+  let env = Global.env () in
+  let evd = Evd.from_env env in
+  let sigma, t = Constrintern.interp_constr_evars env evd e in
+  Feedback.msg_warning (Pp.str "Forgive me sensei, for I must go all out, just this once...");
+  let e = reduce (EConstr.Unsafe.to_constr t) in
+  Feedback.msg_info (Constr.debug_print e)
