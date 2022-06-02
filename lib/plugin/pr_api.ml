0f309879d3a30fd9db712a1be558c25bba249739
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-06-02 04:10:22 +0100

    just this once....

--- lib/plugin/pr_api.ml
+++ lib/plugin/pr_api.ml
@@ -17,8 +17,18 @@ let reduce e =
 let print_reduced (e: Constrexpr.constr_expr_r CAst.t) =
   let env = Global.env () in
   let evd = Evd.from_env env in
-  let sigma, t = Constrintern.interp_constr_evars env evd e in
+  let evd, t = Constrintern.interp_constr_evars env evd e in
   (* Ultimate_reduction *)
   Feedback.msg_warning (Pp.str "Forgive me sensei, for I must go all out, just this once...");
   let (evd, t) = Ultimate_tactics.reduce env evd t in
   Feedback.msg_info (Printer.pr_econstr_env env evd t)
+
+let print_reduced_tac (t: Evd.econstr) =
+  Proofview.Goal.enter @@ fun gl ->
+  let env = Proofview.Goal.env gl in
+  let evd = Proofview.Goal.sigma gl in
+  (* Ultimate_reduction *)
+  Feedback.msg_warning (Pp.str "Forgive me sensei, for I must go all out, just this once...");
+  let (evd, t) = Ultimate_tactics.reduce env evd t in
+  Feedback.msg_info (Printer.pr_econstr_env env evd t);
+  Tacticals.tclIDTAC
