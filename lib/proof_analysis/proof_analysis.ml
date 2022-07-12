b22f4caa03757419e183b2c0f7b4958903491eca
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-07-12 10:09:26 +0100

    temporary commit

--- lib/proof_analysis/proof_analysis.ml
+++ lib/proof_analysis/proof_analysis.ml
@@ -1,10 +1,38 @@
+open Containers
 
-type t = |
+type t = Lol
+
+(** [is_const_wp_fn cst] determines whether a {!Constr.t} term
+   represents a constant weakest precondition helper. *)
+let is_const_wp_fn cst =
+  Constr.isConst cst && begin
+    let cst, _ = Constr.destConst cst  in
+    String.suffix ~suf:"_cf__" @@  Names.Constant.to_string cst
+  end
+
+let rec extract_fold_specification (wp: Constr.t) : t  =
+
+  Format.ksprintf ~f:failwith
+    "extract_fold_specification received %s"
+    (Proof_utils.Debug.constr_to_string wp)
+
   
 let analyse (trm: Constr.t) : t =
   match Constr.kind trm with
-  | Constr.App (trm, [| o1; o2; o3 |]) ->
-    failwith "lollll"
+  | Constr.App (trm, args) when is_const_wp_fn trm && Array.length args > 0 ->
+    let _initial_args = 
+      Array.to_iter args |> Iter.find_map (fun trm ->
+        match Constr.kind trm with
+        | Constr.App (trm, args) when Constr.isVar trm ->
+          Some args
+        | _ ->
+          None
+      ) in
+    let wp = args.(Array.length args - 1) in
+
+    let _fspec = extract_fold_specification wp in
+
+    failwith "lol"
   | _ -> 
     failwith ("lol " ^ Proof_utils.Debug.tag trm)
   
--- resources/seq_to_array/Verify_seq_to_array_new.v
+++ resources/seq_to_array/Verify_seq_to_array_new.v
@@ -27,6 +27,30 @@ Proof using.
     xalloc arr data Hdata.
     xletopaque idx Hidx.
     xletopaque ftmp Hftmp.
+    About list_fold_spec.
+    evar (sym_1: A).
+    evar (sym_2: A).
+    evar (sym_3: A).
+    evar (I: list A -> credits -> hprop).
+    evar (HI: (forall (acc : credits) (v : A) (t r : list A),
+        sym_1 :: sym_2 :: sym_3 :: nil = t ++ v :: r ->
+        SPEC (ftmp acc v)
+        PRE ?I t acc
+        POST (fun acc0 : credits => ?I (t & v) acc0))).
+    pose (list_fold_spec ftmp 3 (cons sym_1 (cons sym_2 (cons sym_3 nil))) ?I ?HI).
+    Print make.
+    Print LibList.make.
+    Eval vm_compute in (drop 1 (cons sym_1 (cons sym_2 (cons sym_3 nil)))).
+  Set Printing All.
+  About CFML.Stdlib.List_ml.fold_left_cf__.
+Print Wpgen_body.
+    Check make.
+    Print Z. int.
+    Set Printing .
+    Check 10.
+
+    Eval compute in (make 10 0).
+    Print make.
     xapp (list_fold_spec ftmp idx rest (fun (t: list A) (i: int) =>
         \[i = length l - length t - 2] \*
         arr ~> Array ((make (i + 1) init) ++ 
