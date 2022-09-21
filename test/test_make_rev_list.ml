be42c0fc2d9a2e27ca25d58243831967d39f7d59
mkeoliya <e0407889@u.nus.edu>
2022-09-21 19:56:38 +0800

    Add test for make_rev_list

---
+++ test/test_make_rev_list.ml
@@ -0,0 +1,10 @@
+module T = Testing_utils.Make (struct let name = "make_rev_list" end)
+
+let () =
+  T.add_test "make_rev_list"
+    (Testing_utils.sisyphus_runs_on
+       ~path:"../resources/make_rev_list"
+       ~coq_name:"ProofsMakeRevList")
+
+let () =
+  Testing_utils.run "make_rev_list_test"
