243e303d6176268e4ca0a87ef1370fc14bbcdf37
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-09-26 08:11:20 +0100

    implemented ideal candidate for proof

--- resources/make_rev_list/Verify_make_rev_list_new.v
+++ resources/make_rev_list/Verify_make_rev_list_new.v
@@ -1,17 +1,25 @@
 Set Implicit Arguments.
 
 From CFML Require Import WPLib Stdlib.
-From TLC Require Import LibListZ.
+From TLC Require Import LibList.
 
 From ProofsMakeRevList Require Import Verify_make_rev_list_utils.
 From ProofsMakeRevList Require Import Make_rev_list_new_ml.
-From ProofsMakeRevList Require Import Common_ml.
 
-Lemma make_rev_list_spec : forall A `{EA:Enc A} (l:list A),
-  SPEC (make_rev_list l)
+Lemma make_rev_list_spec : forall A `{EA:Enc A} (ls:list A),
+  SPEC (make_rev_list ls)
     PRE (\[])
-    POST (fun l' => \[l' = rev l]).
+    POST (fun l' => \[l' = rev ls]).
 Proof using (All).
   xcf.
-  
+  xletopaque tmp Htmp.
+  xapp (ProofsMakeRevList.Verify_make_rev_list_utils.list_fold_spec tmp nil ls
+          (fun (t: list A) (acc: list A) => \[acc = rev t])). {
+    intros acc v t r Hls; apply Htmp.
+    xpullpure Hacc.
+    xvals*.
+    rew_list; rewrite Hacc.
+    auto.
+  } { rew_list. auto. }
+  xvals*.
 Admitted.
