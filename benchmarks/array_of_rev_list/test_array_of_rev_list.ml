10138d3e0b7ab100f1ccecd8b5a0d87b0bc61073
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-10-05 15:37:30 +0100

    fixed obvious typing errors in tests

--- benchmarks/array_of_rev_list/test_array_of_rev_list.ml
+++ benchmarks/array_of_rev_list/test_array_of_rev_list.ml
@@ -4,7 +4,9 @@ let () =
   T.add_test "array_of_rev_list"
     (Benchmark_utils.sisyphus_runs_on
        ~path:"../../resources/array_of_rev_list"
-       ~coq_name:"ProofsArrayOfRevList")
+       ~coq_name:"ProofsArrayOfRevList"
+       ~common_path:"../../resources/common"
+       ~common_coq_name:"Common")
 
 let () =
   Benchmark_utils.run "array_of_rev_list_test"
--- benchmarks/make_rev_list/test_make_rev_list.ml
+++ benchmarks/make_rev_list/test_make_rev_list.ml
@@ -4,7 +4,9 @@ let () =
   T.add_test "make_rev_list"
     (Benchmark_utils.sisyphus_runs_on
        ~path:"../../resources/make_rev_list"
-       ~coq_name:"ProofsMakeRevList")
+       ~coq_name:"ProofsMakeRevList"
+       ~common_path:"../../resources/common"
+       ~common_coq_name:"Common")
 
 let () =
   Benchmark_utils.run "make_rev_list_test"
