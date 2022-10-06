1890308c924e7a6be5d9bdb328e474ca06e0f5ef
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-10-06 09:38:43 +0100

    updated code

---
+++ resources/common/Verify_opt.v
@@ -0,0 +1,33 @@
+Set Implicit Arguments.
+
+From CFML Require Import WPLib Stdlib.
+From TLC Require Import LibListZ.
+
+From Common Require Import Opt_ml.
+
+
+Lemma opt_is_some_spec : forall (A:Type) `{EA: Enc A} (v: option A),
+    SPEC_PURE (option_is_some v)
+      POST (fun (b: bool) => \[b = is_some v]).
+Proof.
+  intros; xcf.
+  xmatch.
+  - xvals*.
+  - xvals*. 
+    unfold Wpgen_negpat in C.
+    destruct v; simpl; auto.
+    pose proof (C a).
+    contradiction H; auto.
+Qed.
+
+Lemma opt_is_none_spec : forall (A:Type) `{EA: Enc A} (v: option A),
+    SPEC_PURE (option_is_none v)
+      POST (fun (b: bool) => \[b = negb (is_some v)]).
+Proof.
+  intros; xcf.
+  xmatch.
+  - xvals*.
+  - xvals*. 
+    unfold Wpgen_negpat in C.
+    destruct v; simpl; auto.
+Qed.
