4a20152855ff0bced502be048a03d2b646521bc4
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-10-13 03:11:04 +0100

    added documentation for types and function to extract polymorphic variables

--- lib/lang/type.ml
+++ lib/lang/type.ml
@@ -1,19 +1,44 @@
 open Containers
 module StringMap = Map.Make(String)
+module StringSet = Set.Make(String)
 
 type t =
   | Unit
+  (** [Unit] represents [(): unit] type.  *)
   | Var of string
+  (** [Var v] represents a polymorphic variable [v]. Note: depending
+     on whether we retrieved this type from OCaml or Coq, the
+     representation of the type variable itself may differ in subtle
+     ways. For example, OCaml type variable strings [v] include the
+     prefixing apostrophe and are always lower case. Coq polymorphic
+     variables have no prefix and are upper case. *)
   | Int
-  | Bool
+  (** [Int] represents the int [1: int] type. *)
+  | Bool 
+  (** [Bool] represents the bool [true: bool] type.  *)
   | Func of (t list * t) option (* optionally include the internal parameters *)
+  (** [Func f] represents a function type - if we know the actual type
+     of the function, then we include it in [f], otherwise [f] is
+     empty. This accounts for the case in which [Func] is used to
+     represent the [func] type from CFML which doesn't track the
+     argument and return types of the function.  *)
   | Loc
+  (** [Loc] represents CFML's [Loc] type  *)
   | List of t
+  (** [List ty] represents a List type over elements of type [ty].  *)
   | Array of t
+  (** [Array ty] represents an Array type over elements of type [ty].
+     *)
   | Ref of t
+  (** [Ref ty] represents a Ref type over elements of type [ty].  *)
   | Product of t list
+  (** [Product tys] represents a product of [tys] types.  *)
   | ADT of string * t list * (string * string) option
+  (** [ADT (name, tys, converters)] represents an arbitrary ADT named
+     [name] instantiated with types [tys], and optionally providing
+     converters [conv]. *)
   | Val
+  (** [Val] represents CFML's opaque Val type.  *)
 [@@deriving show, eq, ord]
 
 let pp_raw = pp
@@ -74,3 +99,23 @@ let rec subst map = function
   | Product elts -> Product (List.map (subst map) elts)
   | ADT (name, args, cons) -> ADT (name, List.map (subst map) args, cons)
   | ty -> ty
+
+let rec poly_vars vars = function
+  | Var v -> StringSet.add v vars
+  | Unit | Int | Bool | Loc | Val -> vars
+  | List ty | Array ty | Ref ty ->  poly_vars vars ty
+  | Func (Some (args, res)) ->
+    let vars = poly_vars vars res in
+    List.fold_left
+      (fun vars ty ->
+         poly_vars vars ty)
+      vars args
+  | Func None -> vars 
+
+  | ADT (_, tys, _) 
+  | Product tys ->
+    List.fold_left
+      (fun vars ty ->
+         poly_vars vars ty) vars tys
+
+
