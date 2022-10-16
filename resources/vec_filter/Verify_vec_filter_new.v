1ae8e5fc6d9ea499907a9ba77c02043c6c7b5395
Kiran Gopinathan <kirangopinathan@pop-os.localdomain>
2022-10-16 23:22:20 +0800

    verified vec filter

--- resources/vec_filter/Verify_vec_filter_new.v
+++ resources/vec_filter/Verify_vec_filter_new.v
@@ -2,100 +2,110 @@ Set Implicit Arguments.
 From CFML Require Import WPLib Stdlib.
 From TLC Require Import LibListZ.
 
-From Common Require Import Verify_sseq.
-From Common Require Import Verify_list.
+From Common Require Import Verify_vec Verify_combinators.
 
-From Common Require Import Tactics.
-From Common Require Import Utils.
+From Common Require Import Tactics Utils Solver.
 
-(* From Proofs Require Import Seq_to_array_new_ml. *)
+From ProofsVecFilter Require Import Vec_filter_new_ml.
 
 
-(* Lemma to_array_spec : *)
-(*   forall (A : Type) `{EA : Enc A} (l : list A) (s : func) (v : loc), *)
-(*   SPEC (to_array s) *)
-(*   PRE \[LSeq l s] *)
-(*   POST (fun a : loc => a ~> Array l). *)
-(* Proof using (All). *)
-(* xcf. *)
-(* xpullpure H1. *)
-(* xletopaque tmp Htmp. *)
-(* evar (ty_ls: Type). *)
-(* evar (ls: ty_ls). *)
-(* xapp (Common.Verify_sseq.fold_spec tmp (0, nil) s ?ls (fun '((i, acc): (int * list (A))) (x: A) => ((i + 1), x :: acc))). *)
-(* sep_solve. *)
-(* eauto. *)
-(* clear ty_ls ls. *)
-(* xdestruct len ls Hlenls. *)
-(* rewrite list_fold_length_rev in Hlenls. *)
-(* injection Hlenls; intros Hls Hlen. *)
-(* case ls as [ | init rest] eqn:H_eqn. *)
-(* - xmatch. *)
-(* xvalemptyarr. *)
-(* { symmetry; apply nil_eq_rev_inv; auto. } *)
-(* - xmatch. *)
-(* xalloc a data Hdata. *)
-(* xletopaque idx Hidx. *)
-(* xletopaque tmp0 Htmp0. *)
-(* xapp (Common.Verify_list.list_fold_spec (tmp0) (idx) (rest) *)
-(*         (fun (arg0: list (A)) *)
-(*              (arg1: int) => *)
-(*            \[ (arg1 = (((length (rest)) + (length (rest))) *)
-(*                        - ((length (l)) + (length (arg0))))) ] *)
-(*              \* a ~> CFML.WPArray.Array ((make (((length (rest)) - (length (arg0)))) *)
-(*                                             (init)) ++ *)
-(*                                            (drop ((length (rest))) ((arg0 ++ l)))))). *)
-(* { intros acc x t r Htxr. *)
-(*   apply Htmp0. *)
-(*   xpull;=> Heq. *)
-(*   xapp. { *)
-(*     apply int_index_prove. *)
-(*     rewrite Heq, <- (length_rev l), <- Hls, !Htxr; rew_list; math. *)
-(*     rewrite <- length_eq, Heq, <- (length_rev l), <- Hls, !Htxr; rew_list. *)
-(*     rewrite length_make; math. *)
-(*   } *)
-(*   xvals*. *)
-(*   rewrite Heq, <- (length_rev l), <- Hls, !Htxr; rew_list; math. *)
-(*   rew_list. *)
-(*   math_rewrite (length rest - length t = (length rest - length t - 1) + 1). *)
-(*   math_rewrite (length rest - (1 + length t) = length rest - length t - 1). *)
-(*   rewrite make_succ_r; try (rewrite Htxr; rew_list; math). *)
-(*   rewrite app_last_l, update_middle; *)
-(*     [ | rewrite Heq, <- (length_rev l), <- Hls,  length_make; rewrite !Htxr; rew_list; math ]. *)
-(*   rewrite Htxr; rew_list. *)
-(*   rewrite drop_app_r; try math. *)
-(*   rewrite drop_app_r; try math. *)
-(*   math_rewrite ((length t + (1 + length r) - length t) = length r + 1). *)
-(*   rewrite drop_cons; try math. *)
-(*   apply (@f_equal (list A) (list A) (@rev A)) in Hls. *)
-(*   rewrite rev_rev, rev_cons, Htxr in Hls; rew_list in Hls. *)
-(*   f_equal. *)
-(*   rewrite <- Hls. *)
-(*   rewrite drop_app_r; rew_list; try math. *)
-(*   math_rewrite ( (length r + 1 - length r) = 0 + 1). *)
-(*   rewrite drop_app_r; rew_list; try math. *)
-(*   math_rewrite ( (length r - length r) = 0). *)
-(*   rewrite drop_cons, !drop_zero; try math. *)
-(*   auto. *)
-(* } *)
-(* { rewrite Hidx, Hlen, <- (length_rev l), <- Hls; rew_list; math. } *)
-(* { rew_list. *)
-(*   math_rewrite (length rest - 0 = length rest). *)
-(*   rewrite Hdata. *)
-(*   apply (@f_equal (list A) (list A) (@rev A)) in Hls. *)
-(*   rewrite rev_rev, rev_cons in Hls; rew_list in Hls. *)
-(*   rewrite Hlen, <- Hls. *)
-(*   rewrite drop_app_r; rew_list; try math. *)
-(*   math_rewrite ((length rest - length rest) = 0). *)
-(*   rewrite drop_zero. *)
-(*   math_rewrite (1 + length rest = length rest + 1). *)
-(*   rewrite make_succ_r; auto. *)
-(*   pose proof (length_nonneg rest); math. *)
-(* } *)
-(* try xdestruct. *)
-(* xvals*. { *)
-(*   rewrite drop_app_r; try math. *)
-(*   math_rewrite ((length rest - length rest) = 0). *)
-(*   rewrite drop_zero, make_zero; auto. *)
-(* } *)
-(* Qed. *)
+Lemma vec_filter_spec (A: Type) `{EA: Enc A}:
+  forall (f: func) (v: vector A)
+         (l: list A) (f_p: A -> bool),
+    (forall (x: A),
+        SPEC_PURE (f x)
+         POST \[= f_p x]
+    ) ->
+    SPEC (vec_filter f v)
+    PRE (v ~> Vector l)
+    POSTUNIT (v ~> Vector (filter f_p l)).
+Proof.
+  xcf.
+  xref j.
+  xapp (@vec_size_spec A EA).
+  xletopaque tmp Htmp.
+  xapp (for_upto_spec 0 (length l) tmp
+          (fun (i: int) =>
+             j ~~> length (filter f_p (take i l)) \*
+               v ~> Vector ((filter f_p (take i l)) ++ drop (length (filter f_p (take i l))) l)
+       )). {
+    sis_solve_start.
+    xapp (@vec_get_spec A EA). {
+      sis_handle_int_index_prove.
+      rewrite length_drop_nonneg; try (rew_list; math).
+      split; try math; pose proof (length_filter_take_leq f_p l i);
+        pose proof (length_filter l f_p);
+        math.
+    }
+    xapp.
+    pose proof (length_filter_take_leq f_p l i);
+      pose proof (length_filter l f_p);
+      pose proof (length_filter (take i l) f_p).
+    rewrite length_take_nonneg in H2; try math.
+    rewrite read_app; try math.
+    rewrite If_r;  try math.
+    rewrite read_drop; try (rew_list; math);
+        try (apply int_index_prove; try math; try (rewrite HD; rew_list; math)).
+    math_rewrite (length (filter (fun x0 : A => f_p x0) (take i l)) +
+               (i - length (filter (fun x0 : A => f_p x0) (take i l))) = i).
+    xif;=> Hfp.
+    - xapp.
+      xapp (@vec_set_spec A EA).  {
+        sis_handle_int_index_prove.
+        rewrite length_drop_nonneg; try (rew_list; math).
+      }
+      xapp.
+      xapp.
+      xvals*. {
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_l; rew_list; auto; math.
+      } {
+        sis_handle_list_update.
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_l; rew_list; auto.
+        f_equal.
+        rewrite drop_write_zero; auto; try math.
+        do 2 f_equal; math.
+      }
+    - xvals*. {
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_r; rew_list; auto; math.
+      } {
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_r; rew_list; auto.
+        do 2 f_equal; math.
+      }
+  } { math. }
+  xmatch.
+  xapp.
+  rewrite !take_full_length.
+  xif;=> cond.
+  - xapp.
+    xval.
+    xif;=> cond2.
+    + xapp (@vec_get_spec A EA). { sis_handle_int_index_prove. }
+      xapp.
+      xapp.
+      xapp (@vec_fill_spec A EA). { eauto. } {
+        instantiate (1:=drop (length (filter (fun x : A => f_p x) l)) l).
+        rewrite length_drop_nonneg; math.
+      } { instantiate (1 := nil); rew_list; auto. }
+      xval.
+      xapp.
+      xapp (@vec_set_size_spec). { auto. }
+      xmatch.
+      xvals*.
+    + xval.
+      xapp.
+      xapp (@vec_set_size_spec). { auto. }
+      xmatch.
+      xvals*.
+  - xval.
+    xif; intros Hv; try (contradiction Hv; auto; math).
+    xval.
+    xapp.
+    xapp (@vec_set_size_spec). { auto. }
+    xmatch.
+    xvals*.
+    Unshelve.  xinhab; auto.
+Qed.
--- resources/vec_filter/Verify_vec_filter_old.v
+++ resources/vec_filter/Verify_vec_filter_old.v
@@ -2,35 +2,85 @@ Set Implicit Arguments.
 From CFML Require Import WPLib Stdlib.
 From TLC Require Import LibListZ.
 
-From Common Require Import Verify_sseq.
+From Common Require Import Verify_vec Verify_combinators.
 
-From Common Require Import Tactics.
-From Common Require Import Utils.
+From Common Require Import Tactics Utils Solver.
 
-(* From Proofs Require Import Seq_to_array_old_ml. *)
+From ProofsVecFilter Require Import Vec_filter_old_ml.
 
-(* Lemma to_array_spec : *)
-(*   forall (A : Type) `{EA : Enc A} (l : list A) (s : func) (v : loc), *)
-(*   SPEC (to_array s) *)
-(*   PRE \[LSeq l s] *)
-(*   POST (fun a : loc => a ~> Array l). *)
-(* Proof using (All). *)
-(*   xcf. *)
-(*   xpullpure HLseq. *)
-(*   apply LSeq_if in HLseq as Hs. *)
-(*   xapp Hs. *)
-(*   intros nxt Hnxt. *)
-(*   case nxt as [ | x nxt2] eqn: H. *)
-(*   - xmatch_case_0. *)
-(*     xvalemptyarr. { admit. } *)
-(*   - xmatch. *)
-(*     xapp (length_spec s l); auto. *)
-(*     (* unification point 1 *) *)
-(*     xalloc arr data Hdata. *)
-(*     xletopaque f Hf. *)
-(*     xapp (iteri_spec f s l *)
-(*                      (fun (ls: list A) => arr ~> Array (ls ++ drop (length ls) (make (length l) x))) *)
-(*          ). { admit. } { admit. } { admit. } *)
-(*     (* unification point 2 *) *)
-(*     xmatch. xvals. { admit. } *)
-(* Admitted. *)
+
+Lemma vec_filter_spec (A: Type) `{EA: Enc A}:
+  forall (f: func) (v: vector A)
+         (l: list A) (f_p: A -> bool),
+    (forall (x: A),
+        SPEC_PURE (f x)
+         POST \[= f_p x]
+    ) ->
+    SPEC (vec_filter f v)
+    PRE (v ~> Vector l)
+    POSTUNIT (v ~> Vector (filter f_p l)).
+Proof.
+  xcf.
+  xref j.
+  xapp (@vec_size_spec A EA).
+  xletopaque tmp Htmp.
+  xapp (for_upto_spec 0 (length l) tmp
+          (fun (i: int) =>
+             j ~~> length (filter f_p (take i l)) \*
+               v ~> Vector ((filter f_p (take i l)) ++ drop (length (filter f_p (take i l))) l)
+       )). {
+    sis_solve_start.
+    xapp (@vec_get_spec A EA). {
+      sis_handle_int_index_prove.
+      rewrite length_drop_nonneg; try (rew_list; math).
+      split; try math; pose proof (length_filter_take_leq f_p l i);
+        pose proof (length_filter l f_p);
+        math.
+    }
+    xapp.
+    pose proof (length_filter_take_leq f_p l i);
+      pose proof (length_filter l f_p);
+      pose proof (length_filter (take i l) f_p).
+    rewrite length_take_nonneg in H2; try math.
+    rewrite read_app; try math.
+    rewrite If_r;  try math.
+    rewrite read_drop; try (rew_list; math);
+        try (apply int_index_prove; try math; try (rewrite HD; rew_list; math)).
+    math_rewrite (length (filter (fun x0 : A => f_p x0) (take i l)) +
+               (i - length (filter (fun x0 : A => f_p x0) (take i l))) = i).
+    xif;=> Hfp.
+    - xapp.
+      xapp (@vec_set_spec A EA).  {
+        sis_handle_int_index_prove.
+        rewrite length_drop_nonneg; try (rew_list; math).
+      }
+      xapp.
+      xapp.
+      xvals*. {
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_l; rew_list; auto; math.
+      } {
+        sis_handle_list_update.
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_l; rew_list; auto.
+        f_equal.
+        rewrite drop_write_zero; auto; try math.
+        do 2 f_equal; math.
+      }
+    - xvals*. {
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_r; rew_list; auto; math.
+      } {
+        rewrite (take_pos_last IA (i + 1)); math_rewrite (i + 1 - 1 = i);
+          try sis_handle_int_index_prove; rewrite filter_last, If_r; rew_list; auto.
+        do 2 f_equal; math.
+      }
+  } { math. }
+  xmatch.
+  xapp.
+  xapp (@vec_set_size_spec). { auto. }
+  xmatch.
+  xvals*. {
+    rewrite take_full_length; auto.
+  }
+Qed.
