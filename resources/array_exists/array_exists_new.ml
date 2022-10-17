1d4c81e7194016cbdb07e15110ea58d7f451515a
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-10-17 08:39:33 +0100

    added array exists lang parsing test

--- resources/array_exists/array_exists_new.ml
+++ resources/array_exists/array_exists_new.ml
@@ -2,7 +2,7 @@ open Combinators
 
 let array_exists (a: 'a array) (f: 'a -> bool) =
   let (len: int) = Array.length a in
-  let (res: bool option) = until_upto 0 len (fun i ->
+  let (res: bool option) = until_upto 0 len (fun (i: int) ->
     if f a.(i)
     then Some true
     else None) in
