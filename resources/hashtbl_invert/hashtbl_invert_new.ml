0d1691d772cf4e49a67cc4efdd4b6b48b0b96e50
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-10-17 08:53:50 +0100

    added hashtbl invert lang parser test

--- resources/hashtbl_invert/hashtbl_invert_new.ml
+++ resources/hashtbl_invert/hashtbl_invert_new.ml
@@ -2,7 +2,7 @@ open Hashtbl
 
 let invert_hashtbl (h: int hashtbl) =
   let (c: int hashtbl) = hashtbl_create () in
-  let (res: int hashtbl) = hashtbl_fold h (fun c ((k,v): int * int) ->
+  let (res: int hashtbl) = hashtbl_fold h (fun (c: int hashtbl) (((k: int),(v: int))) ->
     let (_: unit) = hashtbl_add c v k in
     c
   ) c in
--- resources/hashtbl_invert/hashtbl_invert_old.ml
+++ resources/hashtbl_invert/hashtbl_invert_old.ml
@@ -2,8 +2,8 @@ open Hashtbl
 
 let invert_hashtbl (h: int hashtbl) =
   let (c: int hashtbl) = hashtbl_create () in
-  let (_: unit) = hashtbl_iter h (fun ((k,v): int * int) ->
-    let (_: unit) = hashtbl_add c v k in
+  let (_: unit) = hashtbl_iter h (fun (((k: int),(v : int))) ->
+    let (_: unit) = Hashtbl.hashtbl_add c v k in
     ()
   ) in
   c
