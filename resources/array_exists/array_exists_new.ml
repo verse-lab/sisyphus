421955fa3c659ce3b4813e9419b9b92ea2e3cd71
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-10-18 16:11:23 +0800

    fixed variable shadowing

--- resources/array_exists/array_exists_new.ml
+++ resources/array_exists/array_exists_new.ml
@@ -2,9 +2,9 @@ open Combinators
 
 let array_exists (a: 'a array) (f: 'a -> bool) =
   let (len: int) = Array.length a in
-  let (res: bool option) = until_upto 0 len (fun (i: int) ->
+  let (result: bool option) = until_upto 0 len (fun (i: int) ->
     if f a.(i)
     then Some true
     else None) in
-  let (res: bool) = Opt.option_is_some res in
+  let (res: bool) = Opt.option_is_some result in
   res
