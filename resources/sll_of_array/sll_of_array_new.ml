e0f4448f008bb9c7e751777d2be0a9d2edeb4506
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-10-26 04:30:09 +0100

    work smart, not hard - swapped sll of array new and old

--- resources/sll_of_array/sll_of_array_new.ml
+++ resources/sll_of_array/sll_of_array_new.ml
@@ -1,13 +1,13 @@
-open Combinators
+open Arr
 open Sll
 
 let sll_of_array (a: 'a array) =
   let (s: 'a sll) = sll_nil () in
-  let (len: int) = Array.length a in
-  let (_: unit) = for_downto (len - 1) 0 (fun (i: int) ->
-    let (elt: 'a) = a.(i) in
-    let (_: unit) = Sll.sll_push elt s in
+  let (_: unit) = array_iter (fun (v: 'a) ->
+    let (_: unit) = Sll.sll_push v s in
     ()
-  ) in
+  ) a in
+  let (_: unit) = Sll.sll_reverse s in
   s
 [@@with_opaque_encoding ["sll", ("Sll.sll_of_list", "Sll.sll_to_list")]]
+
--- resources/sll_of_array/sll_of_array_old.ml
+++ resources/sll_of_array/sll_of_array_old.ml
@@ -1,13 +1,14 @@
-open Arr
+open Combinators
 open Sll
 
 let sll_of_array (a: 'a array) =
   let (s: 'a sll) = sll_nil () in
-  let (_: unit) = array_iter (fun (v: 'a) ->
-    let (_: unit) = Sll.sll_push v s in
+  let (len: int) = Array.length a in
+  let (_: unit) = for_downto (len - 1) 0 (fun (i: int) ->
+    let (elt: 'a) = a.(i) in
+    let (_: unit) = Sll.sll_push elt s in
     ()
-  ) a in
-  let (_: unit) = sll_reverse s in
+  ) in
   s
 [@@with_logical_mapping ["ls", "a"]]
 [@@with_opaque_encoding ["sll", ("Sll.sll_of_list", "Sll.sll_to_list")]]
