ae7cae97e70726c29884ae7f9043b83cf0f59291
Mayank Keoliya <mayank@r-244-100-25-172.comp.nus.edu.sg>
2022-11-03 20:28:16 +0800

    Add stat writing

--- benchmarks/table/table.ml
+++ benchmarks/table/table.ml
@@ -1,6 +1,8 @@
 open Containers
 open Bos
 
+module StringSet = Set.Make (String)
+
 type stats = {
   (* sisyphus runtime in seconds *)
   runtime: string;
@@ -25,17 +27,31 @@ let count_admits ~test_dir ~output_name =
   let count = count_substring proof_str "admit." in
   count
 
-let write_to_tex ~table_dir stats =
-  let prelude = IO.with_in "./template.tex" IO.read_all in
-  let postlude = "\\bottomrule\n\\end{tabular}\n\\end{center}" in
-  let stats = Hashtbl.to_list stats
-            |> List.map (fun (name, { runtime; num_admits }) ->
-                  let name = Str.(global_replace (regexp "_") "\_" name) in
-                  Format.sprintf "%s & %s & %s \\\\\n" name runtime (string_of_int num_admits)
-                ) in
-  let stats = List.fold_left ( ^ ) "" stats in
+let parse_file path =
+  let stats = IO.with_in (Fpath.to_string path) IO.read_lines_l
+            |> List.map (String.split_on_char ':')
+            |> List.map (List.map String.trim)
+            |> List.filter_map (function [hd; tl] -> Some (hd, tl) | _ -> None)
+            |> Hashtbl.of_list in
+  stats
 
-  OS.File.write Fpath.(table_dir / "table.tex") (prelude ^ stats ^ postlude)
+let write_to_csv ~table_dir ~headers file_stats =
+  let to_row (name, stats) =
+    List.map (fun key -> Hashtbl.find_opt stats key |> Option.value ~default:"") headers
+    |> List.cons name
+    |> String.concat ","
+  in
+
+  let headers_str = "name" :: headers |> String.concat "," in
+  let row_str =
+    List.to_iter file_stats
+    |> Iter.map to_row
+    |> Iter.intersperse "\n"
+    |> Iter.concat_str in
+
+  let stats_csv = headers_str ^ "\n" ^ row_str in
+
+  OS.File.write Fpath.(table_dir / "stats.csv") (stats_csv)
 
 let build_common ~table_dir common_path common_coq_lib_name =
   let open Result in
@@ -83,34 +99,57 @@ let run_table table_dir common_path common_coq_name (tests: Gen_utils.test_confi
   let* () = build_common ~table_dir common_path common_coq_name in
 
   let stats : (string, stats) Hashtbl.t = Hashtbl.create 14 in
+  let files : (string, Fpath.t) Hashtbl.t = Hashtbl.create 14 in
 
   List.iter (fun ({ name; path; coq_name; common_path =  _; _ } : Gen_utils.test_config) -> (
-      let open Gen_utils in
-      Format.printf "Running %s@." name;
+        let open Gen_utils in
+        Format.printf "Running %s@." name;
 
-      let* path = Fpath.of_string path  in
-      let test_dir = Fpath.add_seg table_dir (Fpath.basename path) in
-      let* _ = OS.Dir.create test_dir in
+        let* path = Fpath.of_string path  in
+        let test_dir = Fpath.add_seg table_dir (Fpath.basename path) in
+        let* _ = OS.Dir.create test_dir in
 
+        let* (deps, old_program, new_program, old_proof_name, output_name, stub_file_name) =
+          build_init ~working_dir:table_dir ~test_dir ~path ~coq_name ~common_path ~common_coq_name ~deps:common_deps in
 
-      let* (deps, old_program, new_program, old_proof_name, output_name, stub_file_name) =
-        build_init ~working_dir:table_dir ~test_dir ~path ~coq_name ~common_path ~common_coq_name ~deps:common_deps in
+        (* export stats file *)
+        let stats_file_name = Format.sprintf "%s_stats.txt" name in
+        let stats_file = Fpath.add_seg table_dir stats_file_name in
 
-      let start_time = Ptime_clock.now () in
-      let* _ = run_sisyphus ~test_dir ~coq_name ~common_dir ~common_coq_name ~deps ~old_program ~new_program ~old_proof_name ~stub_file_name ~output_name in
-      let end_time = Ptime_clock.now () in
-      let runtime = Ptime.diff end_time start_time |> Format.sprintf "%a" Ptime.Span.pp in
+        let start_time = Ptime_clock.now () in
+        let* _ = OS.Env.set_var "SIS_STATS_OUT_FILE" Cmd.(Some (p stats_file)) in
+        let* _ = run_sisyphus ~test_dir ~coq_name ~common_dir ~common_coq_name ~deps ~old_program ~new_program ~old_proof_name ~stub_file_name ~output_name in
+        let end_time = Ptime_clock.now () in
+        let runtime = Ptime.diff end_time start_time |> Format.sprintf "%a" Ptime.Span.pp in
 
-      let* _ = build_res ~working_dir:table_dir ~test_dir ~path ~coq_name ~common_path ~common_coq_name ~output_name in
+        (* _CoqProject file *)
 
-      let num_admits = count_admits ~test_dir ~output_name in
+        let* _ = build_res ~working_dir:table_dir ~test_dir ~path ~coq_name ~common_path ~common_coq_name ~output_name in
 
-      Hashtbl.add stats name { runtime; num_admits };
+        let num_admits = count_admits ~test_dir ~output_name in
 
-      Ok ()
-    ) |> Result.get_exn) tests;
+        Hashtbl.add stats name { runtime; num_admits };
+        Hashtbl.add files name stats_file;
 
-  let* () = write_to_tex ~table_dir stats in
+        Ok ()
+      ) |> Result.get_exn) tests;
+
+  let file_stats = Hashtbl.map_list (fun name path ->
+      let file_stats = parse_file path in
+      let global_stats = Hashtbl.find stats name in
+      Hashtbl.add file_stats "runtime" global_stats.runtime;
+      (name, file_stats)
+    ) files in
+
+  let headers =
+    List.to_iter file_stats
+    |> Iter.map snd
+    |> Iter.flat_map Hashtbl.keys
+    |> Iter.fold (Fun.flip StringSet.add) StringSet.empty
+    |> StringSet.to_list
+  in
+
+  let* () = write_to_csv ~table_dir ~headers file_stats in
 
   Ok ()
 
